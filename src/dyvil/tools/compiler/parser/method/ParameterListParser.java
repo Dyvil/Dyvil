16f34375e9c26e1eb09dfa5e716adc27e8d00400
Clashsoft <clashsoft@hotmail.com>
2014-06-29 18:30:16 +0200

    Parameter Modifiers

--- src/dyvil/tools/compiler/parser/method/ParameterListParser.java
+++ src/dyvil/tools/compiler/parser/method/ParameterListParser.java
@@ -7,16 +7,18 @@ import dyvil.tools.compiler.lexer.token.IToken;
 import dyvil.tools.compiler.parser.Parser;
 import dyvil.tools.compiler.parser.ParserManager;
 import dyvil.tools.compiler.parser.type.TypeParser;
+import dyvil.tools.compiler.util.Modifiers;
 
 public class ParameterListParser extends Parser
 {
-	public static final int	TYPE		= 0;
-	public static final int	NAME		= 1;
-	public static final int	POST_NAME	= 2;
+	public static final int	TYPE	= 0;
+	public static final int	NAME	= 1;
+	public static final int	END		= 2;
 	
 	private IParameterized	parameterized;
 	
 	private Parameter		parameter;
+	private int				modifiers;
 	
 	public ParameterListParser(IParameterized parameterized)
 	{
@@ -26,7 +28,12 @@ public class ParameterListParser extends Parser
 	@Override
 	public boolean parse(ParserManager jcp, String value, IToken token) throws SyntaxError
 	{
-		if (")".equals(value))
+		int i = 0;
+		if ((i = Modifiers.parseParameterModifier(value)) != 0)
+		{
+			this.modifiers |= i;
+		}
+		else if (")".equals(value))
 		{
 			jcp.popParser();
 			this.parameterized.addParameter(this.parameter);
@@ -34,7 +41,7 @@ public class ParameterListParser extends Parser
 		}
 		else if (",".equals(value) || ";".equals(value) || ":".equals(value))
 		{
-			if (this.mode == POST_NAME)
+			if (this.mode == END)
 			{
 				this.parameter.setSeperator(value.charAt(0));
 				this.parameterized.addParameter(this.parameter);
@@ -45,14 +52,16 @@ public class ParameterListParser extends Parser
 		else if (this.mode == TYPE)
 		{
 			this.parameter = new Parameter();
-			jcp.pushParser(new TypeParser(this.parameter), token);
+			this.parameter.setModifiers(this.modifiers);
 			this.mode = NAME;
+			this.modifiers = 0;
+			jcp.pushParser(new TypeParser(this.parameter), token);
 			return true;
 		}
 		else if (this.mode == NAME)
 		{
 			this.parameter.setName(value);
-			this.mode = POST_NAME;
+			this.mode = END;
 		}
 		return false;
 	}
