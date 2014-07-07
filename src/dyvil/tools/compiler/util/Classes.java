e89c0f4dbbd7285d61b2991db8e93c9f58523fa5
Clashsoft <clashsoft@hotmail.com>
2014-07-07 17:54:23 +0200

    Cleanup

--- src/dyvil/tools/compiler/util/Classes.java
+++ src/dyvil/tools/compiler/util/Classes.java
@@ -7,18 +7,24 @@ public class Classes
 	public static final int	INTERFACE	= 3;
 	public static final int	ENUM		= 4;
 	public static final int	ANNOTATION	= 5;
-	public static final int MODULE		= 6;
+	public static final int	MODULE		= 6;
 	
 	public static String toString(int type)
 	{
 		switch (type)
 		{
-		case CLASS: return "class";
-		case OBJECT: return "object";
-		case INTERFACE: return "interface";
-		case ENUM: return "enum";
-		case ANNOTATION: return "annotation";
-		case MODULE: return "module";
+		case CLASS:
+			return "class";
+		case OBJECT:
+			return "object";
+		case INTERFACE:
+			return "interface";
+		case ENUM:
+			return "enum";
+		case ANNOTATION:
+			return "annotation";
+		case MODULE:
+			return "module";
 		}
 		return "";
 	}
@@ -27,13 +33,19 @@ public class Classes
 	{
 		switch (type)
 		{
-		case "class": return CLASS;
-		case "object": return OBJECT;
-		case "interface": return INTERFACE;
-		case "enum": return ENUM;
-		case "annotation": return ANNOTATION;
-		case "module": return MODULE;
+		case "class":
+			return CLASS;
+		case "object":
+			return OBJECT;
+		case "interface":
+			return INTERFACE;
+		case "enum":
+			return ENUM;
+		case "annotation":
+			return ANNOTATION;
+		case "module":
+			return MODULE;
 		}
 		return -1;
-	}	
+	}
 }
