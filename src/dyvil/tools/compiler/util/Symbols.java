0ba43b056d50dd6cbd3d4541be39982d8f23449d
Clashsoft <clashsoft@hotmail.com>
2014-07-18 19:09:22 +0200

    Update Symbols

--- src/dyvil/tools/compiler/util/Symbols.java
+++ src/dyvil/tools/compiler/util/Symbols.java
@@ -6,67 +6,24 @@ public class Symbols
 	{
 		switch (s)
 		{
-		case "=":
-			return "$set";
-			
-		case "==":
-			return "$eq";
-		case "!=":
-			return "$ue";
-		case "<":
-			return "$st";
-		case "<=":
-			return "$se";
-		case ">":
-			return "$gt";
-		case ">=":
-			return "$ge";
-			
-		case "->":
-			return "$arr";
-		case "<-":
-			return "$arl";
-		case "=>":
-			return "$cst";
-		case ":>":
-			return "$iof";
-			
-		case "+":
-			return "$add";
-		case "-":
-			return "$sub";
-		case "*":
-			return "$mul";
-		case "/":
-			return "$div";
-		case "%":
-			return "$mod";
-			
-		case "&":
-			return "$and";
-		case "|":
-			return "$or";
-		case "^":
-			return "$xor";
-			
-		case "<<":
-			return "$bsl";
-		case ">>":
-			return "$bsr";
-		case ">>>":
-			return "$usr";
-			
-		case ".-":
-			return "$neg";
-		case "~":
-			return "$inv";
-		case "++":
-			return "$inc";
-		case "--":
-			return "$dec";
-		case "**":
-			return "$sqr";
-			// case "//": return "$rec";
+		case "=": return "$eq";
+		case ">": return "$greater";
+		case "<": return "$less";
+		case "+": return "$plus";
+		case "-": return "$minus";
+		case "*": return "$times";
+		case "/": return "$div";
+		case "!": return "$bang";
+		case "@": return "$at";
+		case "#": return "$hash";
+		case "%": return "$percent";
+		case "^": return "$up";
+		case "&": return "$amp";
+		case "~": return "$tilde";
+		case "?": return "$qmark";
+		case "|": return "$bar";
+		case "\\": return "$bslash";
+		case ":": return "$colon";
 		}
 		return s;
 	}
@@ -75,83 +32,24 @@ public class Symbols
 	{
 		switch (s)
 		{
-		case "$set":
-		case "set":
-			return "=";
-			
-		case "$eq":
-			return "==";
-		case "$ue":
-			return "!=";
-		case "$st":
-			return "<";
-		case "$se":
-			return "<=";
-		case "$gt":
-			return ">";
-		case "$ge":
-			return ">=";
-			
-		case "$arr":
-			return "->";
-		case "$arl":
-			return "<-";
-		case "$cst":
-			return "=>";
-		case "$iof":
-			return ":>";
-			
-		case "$add":
-		case "add":
-			return "+";
-		case "$sub":
-		case "substract":
-		case "remove":
-			return "-";
-		case "$mul":
-		case "multiply":
-			return "*";
-		case "$div":
-		case "divide":
-			return "/";
-		case "$mod":
-		case "modulo":
-			return "%";
-			
-		case "$and":
-		case "and":
-			return "&";
-		case "$or":
-		case "or":
-			return "|";
-		case "$xor":
-		case "xor":
-			return "^";
-			
-		case "$bsl":
-			return "<<";
-		case "$bsr":
-			return ">>";
-		case "$usr":
-			return ">>>";
-			
-		case "$neg":
-		case "negate":
-			return "-";
-		case "$inv":
-		case "invert":
-			return "~";
-		case "$inc":
-		case "increment":
-			return "++";
-		case "$dec":
-		case "decrement":
-			return "--";
-		case "$sqr":
-		case "square":
-			return "**";
-			
-			// case "$rev": return "//";
+		case "$eq": return "=";
+		case "$greater": return ">";
+		case "$less": return "<";
+		case "$plus": return "+";
+		case "$minus": return "-";
+		case "$times": return "*";
+		case "$div": return "/";
+		case "$bang": return "!";
+		case "$at": return "@";
+		case "$hash": return "#";
+		case "$percent": return "%";
+		case "$up": return "^";
+		case "$amp": return "&";
+		case "$tilde": return "~";
+		case "$qmark": return "?";
+		case "$bar": return "|";
+		case "$bslash": return "\\";
+		case "$colon": return ":";
 		}
 		return s;
 	}
