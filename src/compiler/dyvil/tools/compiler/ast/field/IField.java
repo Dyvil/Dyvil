6fd3c21dc0dbe16d39a3b9937ad508d89733808f
Clashsoft <clashsoft@hotmail.com>
2015-06-27 17:21:56 +0200

    Re-added IField

---
+++ src/compiler/dyvil/tools/compiler/ast/field/IField.java
@@ -0,0 +1,9 @@
+package dyvil.tools.compiler.ast.field;
+
+import dyvil.tools.compiler.ast.member.IClassCompilable;
+import dyvil.tools.compiler.ast.member.IClassMember;
+
+public interface IField extends IClassMember, IDataMember, IClassCompilable
+{
+	
+}
