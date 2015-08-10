aa670fc73f55a4e0b331240d220e03f4d52060aa
Clashsoft <clashsoft@hotmail.com>
2015-08-10 22:55:33 +0200

    Re-add dyvil.collection.mutable.TreeMap
    Re-added the class because it has been removed by Git in the previous
    commit.

---
+++ src/library/dyvil/collection/mutable/TreeMap.java
@@ -0,0 +1,176 @@
+package dyvil.collection.mutable;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+
+import dyvil.collection.ImmutableMap;
+import dyvil.collection.Map;
+import dyvil.collection.MutableMap;
+
+public class TreeMap<K, V> extends AbstractTreeMap<K, V> implements MutableMap<K, V>
+{
+	public TreeMap()
+	{
+		super();
+	}
+	
+	public TreeMap(Comparator<? super K> comparator)
+	{
+		super(comparator);
+	}
+	
+	public TreeMap(Map<? extends K, ? extends V> m)
+	{
+		super(m, null);
+	}
+	
+	public TreeMap(Map<? extends K, ? extends V> m, Comparator<? super K> comparator)
+	{
+		super(m, comparator);
+	}
+	
+	@Override
+	public void clear()
+	{
+		this.size = 0;
+		this.root = null;
+	}
+	
+	@Override
+	public V put(K key, V value)
+	{
+		return this.putUnsafe(key, value);
+	}
+	
+	@Override
+	public boolean putIfAbsent(K key, V value)
+	{
+		if (this.contains(key, value))
+		{
+			return false;
+		}
+		
+		this.putUnsafe(key, value);
+		return true;
+	}
+	
+	@Override
+	public boolean replace(K key, V oldValue, V newValue)
+	{
+		TreeEntry<K, V> p = this.getEntry(key);
+		if (p != null && Objects.equals(oldValue, p.value))
+		{
+			p.value = newValue;
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public V replace(K key, V value)
+	{
+		TreeEntry<K, V> p = this.getEntry(key);
+		if (p != null)
+		{
+			V oldValue = p.value;
+			p.value = value;
+			return oldValue;
+		}
+		return null;
+	}
+	
+	@Override
+	public V removeKey(Object key)
+	{
+		TreeEntry<K, V> entry = this.getEntry(key);
+		if (entry == null)
+		{
+			return null;
+		}
+		
+		V value = entry.value;
+		this.deleteEntry(entry);
+		return value;
+	}
+	
+	@Override
+	public boolean removeValue(Object value)
+	{
+		for (TreeEntry<K, V> e = this.getFirstEntry(); e != null; e = successor(e))
+		{
+			if (Objects.equals(value, e.value))
+			{
+				this.deleteEntry(e);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean remove(Object key, Object value)
+	{
+		TreeEntry<K, V> entry = this.getEntry(key);
+		if (entry == null || !Objects.equals(value, entry.value))
+		{
+			return false;
+		}
+		
+		this.deleteEntry(entry);
+		return true;
+	}
+	
+	@Override
+	public void forEach(BiConsumer<? super K, ? super V> action)
+	{
+		for (TreeEntry<K, V> e = this.getFirstEntry(); e != null; e = successor(e))
+		{
+			action.accept(e.key, e.value);
+		}
+	}
+	
+	@Override
+	public void map(BiFunction<? super K, ? super V, ? extends V> function)
+	{
+		for (TreeEntry<K, V> e = this.getFirstEntry(); e != null; e = successor(e))
+		{
+			e.value = function.apply(e.key, e.value);
+		}
+	}
+	
+	@Override
+	public void filter(BiPredicate<? super K, ? super V> condition)
+	{
+		TreeEntry<K, V> e = this.getFirstEntry();
+		while (e != null)
+		{
+			TreeEntry<K, V> next = successor(e);
+			if (!condition.test(e.key, e.value))
+			{
+				this.deleteEntry(e);
+			}
+			e = next;
+		}
+	}
+	
+	@Override
+	public MutableMap<K, V> copy()
+	{
+		return new TreeMap(this, this.comparator);
+	}
+	
+	@Override
+	public <RK, RV> MutableMap<RK, RV> emptyCopy()
+	{
+		return new TreeMap(this.comparator);
+	}
+	
+	@Override
+	public ImmutableMap<K, V> immutable()
+	{
+		return new dyvil.collection.immutable.TreeMap(this, this.comparator);
+	}
+}
