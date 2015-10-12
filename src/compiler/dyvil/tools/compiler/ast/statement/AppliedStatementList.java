637e964efcefa3129057b72e58737ad5e231370e
Clashsoft <clashsoft@hotmail.com>
2015-10-12 12:50:15 +0300

    Fix Applied Statement Lists
    Temporarily fixed Applied Statement Lists working incorrectly by
    removing their special behavior.

--- src/compiler/dyvil/tools/compiler/ast/statement/AppliedStatementList.java
+++ src/compiler/dyvil/tools/compiler/ast/statement/AppliedStatementList.java
@@ -1,10 +1,5 @@
 package dyvil.tools.compiler.ast.statement;
 
-import dyvil.tools.compiler.ast.context.IContext;
-import dyvil.tools.compiler.ast.expression.IValue;
-import dyvil.tools.compiler.ast.generic.ITypeContext;
-import dyvil.tools.compiler.ast.type.IType;
-import dyvil.tools.compiler.lexer.marker.MarkerList;
 import dyvil.tools.compiler.lexer.position.ICodePosition;
 
 public class AppliedStatementList extends StatementList
@@ -16,20 +11,5 @@ public class AppliedStatementList extends StatementList
 	public AppliedStatementList(ICodePosition position)
 	{
 		this.position = position;
-	}
-	
-	@Override
-	public IValue withType(IType type, ITypeContext typeContext, MarkerList markers, IContext context)
-	{
-		super.resolve(markers, context);
-		
-		return super.withType(type, typeContext, markers, context);
-	}
-	
-	@Override
-	public IValue resolve(MarkerList markers, IContext context)
-	{
-		// Values are resolved in withType
-		return this;
 	}
 }
