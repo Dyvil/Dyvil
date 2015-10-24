980d9cb9697757316290a51eec515b37607b2a38
Clashsoft <clashsoft@hotmail.com>
2015-10-24 23:05:37 +0200

    Update build.gradle
    Updated the Gradle Build Script. The new build script now replaces
    version tokens in the code automatically and also generates a version
    file.

--- src/compiler/dyvil/tools/compiler/DyvilCompiler.java
+++ src/compiler/dyvil/tools/compiler/DyvilCompiler.java
@@ -24,9 +24,9 @@ import dyvil.tools.parsing.CodeFile;
 
 public final class DyvilCompiler
 {
-	public static final String	VERSION			= "0.7.0";
-	public static final String	DYVIL_VERSION	= "0.7.0";
-	public static final String	LIBRARY_VERSION	= "0.6.0";
+	public static final String	VERSION			= "$$version$$";
+	public static final String	DYVIL_VERSION	= "$$compilerVersion$$";
+	public static final String	LIBRARY_VERSION	= "$$libraryVersion$$";
 	
 	public static boolean	debug;
 	public static int		constantFolding	= 2;
--- src/repl/dyvil/tools/repl/DyvilREPL.java
+++ src/repl/dyvil/tools/repl/DyvilREPL.java
@@ -24,7 +24,7 @@ import dyvil.tools.repl.command.*;
 
 public class DyvilREPL
 {
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "$$replVersion$$";
 	
 	private static BufferedReader	reader;
 	protected static REPLContext	context	= new REPLContext();
