b9ef39e3f994a77144e2570980cdf8c9cb959122
Clashsoft <clashsoft@hotmail.com>
2015-11-29 23:15:31 +0100

    Event API
    Added a basic Event API with Invariant and Covariant Dispatch support.
    Closes #141

---
+++ src/library/dyvil/event/HandlerEntry.java
@@ -0,0 +1,39 @@
+package dyvil.event;
+
+import dyvil.collection.Map;
+import dyvil.collection.mutable.HashMap;
+
+import java.lang.reflect.Method;
+
+public class HandlerEntry
+{
+	private final Object   eventHandler;
+	private final Class<?> type;
+	private final Map<Class<?>, Method> targetMethods = new HashMap<>();
+
+	public HandlerEntry(Object eventHandler, Class<?> type)
+	{
+		this.eventHandler = eventHandler;
+		this.type = type;
+	}
+
+	public void addMapping(Class<?> type, Method target)
+	{
+		this.targetMethods.put(type, target);
+	}
+
+	public Object getEventHandler()
+	{
+		return eventHandler;
+	}
+
+	public Class<?> getType()
+	{
+		return type;
+	}
+
+	public Method getTargetMethod(Class<?> type)
+	{
+		return this.targetMethods.get(type);
+	}
+}
