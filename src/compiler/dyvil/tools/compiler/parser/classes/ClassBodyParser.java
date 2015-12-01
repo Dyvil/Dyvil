e8b56d4b2fbd6d86efaec4a2cf589e121ad17b63
Clashsoft <clashsoft@hotmail.com>
2015-12-01 16:17:14 +0100

    Operator Keyword as part of Method Names
    Added support for prefixing method names with the `operator` keyword.
    Closes #146

--- src/compiler/dyvil/tools/compiler/parser/classes/ClassBodyParser.java
+++ src/compiler/dyvil/tools/compiler/parser/classes/ClassBodyParser.java
@@ -30,22 +30,23 @@ import dyvil.tools.parsing.token.IToken;
 
 public final class ClassBodyParser extends Parser implements ITypeConsumer
 {
-	public static final int	TYPE			= 1;
-	public static final int	NAME			= 2;
-	public static final int	GENERICS_END	= 4;
-	public static final int	PARAMETERS		= 8;
-	public static final int	PARAMETERS_END	= 16;
-	public static final int	FIELD_END		= 32;
-	public static final int	PROPERTY_END	= 64;
-	public static final int	METHOD_VALUE	= 128;
-	public static final int	METHOD_END		= 256;
+	protected static final int TYPE           = 1;
+	protected static final int NAME_OPERATOR  = 2;
+	protected static final int NAME           = 4;
+	protected static final int GENERICS_END   = 8;
+	protected static final int PARAMETERS     = 16;
+	protected static final int PARAMETERS_END = 32;
+	protected static final int FIELD_END      = 64;
+	protected static final int PROPERTY_END   = 128;
+	protected static final int METHOD_VALUE   = 256;
+	protected static final int METHOD_END     = 512;
 	
-	protected IClass				theClass;
-	protected IClassBodyConsumer	consumer;
+	protected IClass             theClass;
+	protected IClassBodyConsumer consumer;
 	
-	private IType			type;
-	private int				modifiers;
-	private AnnotationList	annotations;
+	private IType          type;
+	private int            modifiers;
+	private AnnotationList annotations;
 	
 	private IMember member;
 	
@@ -108,7 +109,7 @@ public final class ClassBodyParser extends Parser implements ITypeConsumer
 				this.mode = PARAMETERS;
 				return;
 			}
-			int i = 0;
+			int i;
 			if ((i = ModifierTypes.MEMBER.parse(type)) != -1)
 			{
 				this.modifiers |= i;
@@ -123,7 +124,8 @@ public final class ClassBodyParser extends Parser implements ITypeConsumer
 					return;
 				}
 				
-				ClassDeclarationParser parser = new ClassDeclarationParser(this.theClass, this.modifiers | i, this.annotations);
+				ClassDeclarationParser parser = new ClassDeclarationParser(this.theClass, this.modifiers | i,
+				                                                           this.annotations);
 				pm.pushParser(parser);
 				this.reset();
 				return;
@@ -147,8 +149,15 @@ public final class ClassBodyParser extends Parser implements ITypeConsumer
 				return;
 			}
 			pm.pushParser(pm.newTypeParser(this), true);
-			this.mode = NAME;
+			this.mode = NAME_OPERATOR;
 			return;
+		case NAME_OPERATOR:
+			if (type == DyvilKeywords.OPERATOR)
+			{
+				this.mode = NAME;
+				return;
+			}
+			// Fallthrough
 		case NAME:
 			if (!ParserUtil.isIdentifier(type))
 			{
@@ -178,7 +187,8 @@ public final class ClassBodyParser extends Parser implements ITypeConsumer
 			{
 				this.mode = PARAMETERS;
 				
-				AbstractMethod m = new CodeMethod(token.raw(), this.theClass, token.nameValue(), this.type, this.modifiers);
+				AbstractMethod m = new CodeMethod(token.raw(), this.theClass, token.nameValue(), this.type,
+				                                  this.modifiers);
 				m.setAnnotations(this.annotations);
 				this.member = m;
 				return;
