da91de3d4287c79ca6ffbdbe9a753824d2513853
Clashsoft <clashsoft@hotmail.com>
2016-02-06 13:07:56 +0100

    Add ANSI Color Codes to dyvil.io.Console
    - Added the ANSI Color codes as constants to the `dyvil.io.Console`
    class.

--- src/library/dyvil/io/Console.java
+++ src/library/dyvil/io/Console.java
@@ -5,12 +5,29 @@ import java.util.function.BooleanSupplier;
 
 public final class Console
 {
+	public static final char ANSI_ESCAPE = '\u001B';
+
+	public static final String ANSI_RESET  = "\u001B[0m";
+	public static final String ANSI_BLACK  = "\u001B[30m";
+	public static final String ANSI_RED    = "\u001B[31m";
+	public static final String ANSI_GREEN  = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE   = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN   = "\u001B[36m";
+	public static final String ANSI_WHITE  = "\u001B[37m";
+
 	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private static PrintStream    out    = System.out;
 	
 	private Console()
 	{
 
+	}
+
+	public static boolean isTerminalOutput()
+	{
+		return System.console() != null;
 	}
 	
 	public static void setOut(PrintStream out)
