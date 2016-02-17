fcc109a91914f78416593a4a91228a654f57292f
Clashsoft <clashsoft@hotmail.com>
2016-02-17 21:15:49 +0100

    Split Predef into multiple classes
    - Split the large Predef class into multiple smaller components.
    - Added the `@DyvilType` annotation.
    
    All new component classes are automatically imported by any Dyvil
    source file through the Lang Header.

---
+++ src/library/dyvil/annotation/_internal/DyvilType.java
@@ -0,0 +1,13 @@
+package dyvil.annotation._internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+public @interface DyvilType
+{
+	String value();
+}
