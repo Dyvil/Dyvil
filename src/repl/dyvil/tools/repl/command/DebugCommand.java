5a57f72cc6d6c138fc38bd679b4533da2366c159
Clashsoft <clashsoft@hotmail.com>
2016-02-25 20:32:57 +0100

    Update :debug Command
    - The `:debug` command now uses an optional argument to enable or
    disable Debug Mode.

--- src/repl/dyvil/tools/repl/command/DebugCommand.java
+++ src/repl/dyvil/tools/repl/command/DebugCommand.java
@@ -26,7 +26,32 @@ public class DebugCommand implements ICommand
 	@Override
 	public void execute(DyvilREPL repl, String... args)
 	{
-		DyvilCompiler.debug = !DyvilCompiler.debug;
-		repl.getOutput().println("Setting debug mode to " + (DyvilCompiler.debug ? "ON" : "OFF"));
+		final boolean enableDebug;
+		if (args.length > 0)
+		{
+			switch (args[0].toLowerCase())
+			{
+			case "on":
+			case "true":
+			case "enable":
+				enableDebug = true;
+				break;
+			case "off":
+			case "false":
+			case "disable":
+				enableDebug = false;
+				break;
+			default:
+				repl.getErrorOutput().println("Invalid Argument. Usage: " + this.getUsage());
+				return;
+			}
+		}
+		else
+		{
+			enableDebug = !DyvilCompiler.debug;
+		}
+
+		DyvilCompiler.debug = enableDebug;
+		repl.getOutput().println("Setting debug mode to " + (enableDebug ? "ON" : "OFF"));
 	}
 }
