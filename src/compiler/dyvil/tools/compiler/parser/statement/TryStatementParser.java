d17b2866e2fd7be3a0fc4ebc1adc95eb77e069fe
Clashsoft <clashsoft@hotmail.com>
2016-03-22 23:26:57 +0100

    Optional Catch Parenthesis
    - Made Parenthesis optional for Catch Blocks. #224

--- src/compiler/dyvil/tools/compiler/parser/statement/TryStatementParser.java
+++ src/compiler/dyvil/tools/compiler/parser/statement/TryStatementParser.java
@@ -7,6 +7,7 @@ import dyvil.tools.compiler.ast.statement.exception.TryStatement;
 import dyvil.tools.compiler.parser.IParserManager;
 import dyvil.tools.compiler.parser.Parser;
 import dyvil.tools.compiler.parser.ParserUtil;
+import dyvil.tools.compiler.parser.expression.ExpressionParser;
 import dyvil.tools.compiler.transform.DyvilKeywords;
 import dyvil.tools.parsing.lexer.BaseSymbols;
 import dyvil.tools.parsing.lexer.Tokens;
@@ -14,21 +15,21 @@ import dyvil.tools.parsing.token.IToken;
 
 public final class TryStatementParser extends Parser implements IValueConsumer
 {
-	private static final int ACTION      = 1;
-	private static final int CATCH       = 2;
-	private static final int CATCH_OPEN  = 4;
-	private static final int CATCH_VAR   = 16;
-	private static final int CATCH_CLOSE = 32;
-	
+	private static final int ACTION          = 1;
+	private static final int CATCH           = 2;
+	private static final int CATCH_OPEN      = 4;
+	private static final int CATCH_CLOSE     = 8;
+	private static final int CATCH_SEPARATOR = 16;
+
 	protected TryStatement statement;
 	private   CatchBlock   catchBlock;
-	
+
 	public TryStatementParser(TryStatement statement)
 	{
 		this.statement = statement;
 		this.mode = ACTION;
 	}
-	
+
 	@Override
 	public void parse(IParserManager pm, IToken token)
 	{
@@ -71,12 +72,15 @@ public final class TryStatementParser extends Parser implements IValueConsumer
 			pm.popParser(true);
 			return;
 		case CATCH_OPEN:
-			this.mode = CATCH_VAR;
-			pm.pushParser(new VariableParser(this.catchBlock));
-			if (type != BaseSymbols.OPEN_PARENTHESIS)
+			if (type == BaseSymbols.OPEN_PARENTHESIS)
 			{
-				pm.reparse();
-				pm.report(token, "try.catch.open_paren");
+				this.mode = CATCH_CLOSE;
+				pm.pushParser(new VariableParser(this.catchBlock));
+			}
+			else
+			{
+				this.mode = CATCH_SEPARATOR;
+				pm.pushParser(new VariableParser(this.catchBlock), true);
 			}
 			return;
 		case CATCH_CLOSE:
@@ -87,9 +91,30 @@ public final class TryStatementParser extends Parser implements IValueConsumer
 				pm.reparse();
 				pm.report(token, "try.catch.close_paren");
 			}
+			return;
+		case CATCH_SEPARATOR:
+			switch (type)
+			{
+			case Tokens.EOF:
+				pm.popParser();
+				return;
+			case BaseSymbols.SEMICOLON:
+				this.mode = CATCH;
+				return;
+			case BaseSymbols.COLON:
+				this.mode = CATCH;
+				pm.pushParser(new ExpressionParser(this.catchBlock));
+				return;
+			case BaseSymbols.OPEN_CURLY_BRACKET:
+				this.mode = CATCH;
+				pm.pushParser(new StatementListParser(this.catchBlock), true);
+				return;
+			}
+
+			pm.report(token, "try.catch.separator");
 		}
 	}
-	
+
 	@Override
 	public void setValue(IValue value)
 	{
