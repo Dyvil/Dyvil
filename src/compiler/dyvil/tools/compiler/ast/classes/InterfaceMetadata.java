0b4a46aade40bb2648406ef746ad76628355ded3
Clashsoft <clashsoft@hotmail.com>
2016-03-23 19:28:34 +0100

    Default Methods in Interfaces
    - Interfaces can now have methods with default implementations.
    - Interfaces and Traits now mandate warnings for members that are
    explicitly declared `public`.
    - Interfaces now mandate errors for Properties with initializers.

--- src/compiler/dyvil/tools/compiler/ast/classes/InterfaceMetadata.java
+++ src/compiler/dyvil/tools/compiler/ast/classes/InterfaceMetadata.java
@@ -4,10 +4,15 @@ import dyvil.reflect.Modifiers;
 import dyvil.tools.compiler.ast.context.IContext;
 import dyvil.tools.compiler.ast.external.ExternalClass;
 import dyvil.tools.compiler.ast.field.IField;
+import dyvil.tools.compiler.ast.field.IProperty;
+import dyvil.tools.compiler.ast.member.IMember;
 import dyvil.tools.compiler.ast.method.IMethod;
+import dyvil.tools.compiler.ast.modifiers.BaseModifiers;
 import dyvil.tools.compiler.backend.ClassWriter;
 import dyvil.tools.compiler.backend.exception.BytecodeException;
+import dyvil.tools.compiler.util.Markers;
 import dyvil.tools.parsing.marker.MarkerList;
+import dyvil.tools.parsing.position.ICodePosition;
 
 public class InterfaceMetadata implements IClassMetadata
 {
@@ -32,35 +37,84 @@ public class InterfaceMetadata implements IClassMetadata
 			return;
 		}
 
-		// Make fields public and static
 		for (int i = 0, count = classBody.fieldCount(); i < count; i++)
 		{
-			processField(classBody.getField(i));
+			this.processField(classBody.getField(i), markers);
 		}
 
-		// Make non-static methods public and abstract
 		for (int i = 0, count = classBody.methodCount(); i < count; i++)
 		{
-			processMethod(classBody.getMethod(i));
+			this.processMethod(classBody.getMethod(i), markers);
 		}
-	}
 
-	protected static void processMethod(IMethod method)
-	{
-		if (!method.hasModifier(Modifiers.STATIC))
+		for (int i = 0, count = classBody.propertyCount(); i < count; i++)
 		{
-			method.getModifiers().addIntModifier(Modifiers.PUBLIC | Modifiers.ABSTRACT);
+			this.processProperty(classBody.getProperty(i), markers);
 		}
 	}
 
-	protected static void processField(IField field)
+	protected void processMember(IMember member, MarkerList markers)
 	{
+		if (!member.hasModifier(Modifiers.PUBLIC))
+		{
+			// Make all members public
+			member.getModifiers().addIntModifier(Modifiers.PUBLIC);
+		}
+		else if (member.getModifiers().hasModifier(BaseModifiers.PUBLIC))
+		{
+			markers.add(Markers.semantic(member.getPosition(), "interface.member.public", member.getName()));
+		}
+	}
+
+	protected void processMethod(IMethod method, MarkerList markers)
+	{
+		this.processMember(method, markers);
+
+		if (!method.hasModifier(Modifiers.STATIC) && method.getValue() == null)
+		{
+			// Make methods without an implementation abstract
+			method.getModifiers().addIntModifier(Modifiers.ABSTRACT);
+		}
+	}
+
+	protected void processField(IField field, MarkerList markers)
+	{
+		this.processMember(field, markers);
+
 		if (field.isField())
 		{
-			field.getModifiers().addIntModifier(Modifiers.PUBLIC | Modifiers.STATIC | Modifiers.FINAL);
+			field.getModifiers().addIntModifier(Modifiers.STATIC | Modifiers.FINAL);
 		}
 	}
-	
+
+	protected void processProperty(IProperty property, MarkerList markers)
+	{
+		this.processMember(property, markers);
+
+		final IMethod getter = property.getGetter();
+		if (getter != null)
+		{
+			this.processMethod(getter, markers);
+		}
+
+		final IMethod setter = property.getGetter();
+		if (setter != null)
+		{
+			this.processMethod(setter, markers);
+		}
+
+		final ICodePosition initializerPosition = property.getInitializerPosition();
+		if (initializerPosition != null)
+		{
+			this.processPropertyInitializer(initializerPosition, markers);
+		}
+	}
+
+	protected void processPropertyInitializer(ICodePosition position, MarkerList markers)
+	{
+		markers.add(Markers.semanticError(position, "interface.property.initializer"));
+	}
+
 	@Override
 	public void write(ClassWriter writer) throws BytecodeException
 	{
--- src/compiler/dyvil/tools/compiler/ast/classes/TraitMetadata.java
+++ src/compiler/dyvil/tools/compiler/ast/classes/TraitMetadata.java
@@ -1,73 +1,26 @@
 package dyvil.tools.compiler.ast.classes;
 
 import dyvil.reflect.Modifiers;
-import dyvil.tools.compiler.ast.context.IContext;
-import dyvil.tools.compiler.ast.external.ExternalClass;
 import dyvil.tools.compiler.ast.field.IField;
-import dyvil.tools.compiler.ast.method.IMethod;
-import dyvil.tools.compiler.ast.modifiers.ModifierSet;
-import dyvil.tools.compiler.backend.ClassWriter;
-import dyvil.tools.compiler.backend.exception.BytecodeException;
 import dyvil.tools.compiler.util.Markers;
 import dyvil.tools.parsing.marker.MarkerList;
 
-public class TraitMetadata implements IClassMetadata
+public class TraitMetadata extends InterfaceMetadata
 {
-	private final IClass theClass;
-
 	public TraitMetadata(IClass theClass)
 	{
-		this.theClass = theClass;
+		super(theClass);
 	}
 
 	@Override
-	public void resolveTypesHeader(MarkerList markers, IContext context)
+	protected void processField(IField field, MarkerList markers)
 	{
-		if (this.theClass instanceof ExternalClass)
-		{
-			return;
-		}
+		this.processMember(field, markers);
 
-		final IClassBody classBody = this.theClass.getBody();
-		if (classBody == null)
+		if (!field.hasModifier(Modifiers.STATIC) || !field.hasModifier(Modifiers.FINAL))
 		{
-			return;
+			field.getModifiers().addIntModifier(Modifiers.STATIC | Modifiers.FINAL);
+			markers.add(Markers.semanticWarning(field.getPosition(), "trait.field.warning", field.getName()));
 		}
-
-		for (int i = 0, count = classBody.fieldCount(); i < count; i++)
-		{
-			final IField field = classBody.getField(i);
-			if (!field.isField())
-			{
-				// Skip properties
-				continue;
-			}
-
-			final ModifierSet modifierSet = field.getModifiers();
-			if (!modifierSet.hasIntModifier(Modifiers.PUBLIC))
-			{
-				modifierSet.addIntModifier(Modifiers.PUBLIC);
-			}
-			if (!modifierSet.hasIntModifier(Modifiers.STATIC) || !modifierSet.hasIntModifier(Modifiers.FINAL))
-			{
-				modifierSet.addIntModifier(Modifiers.STATIC);
-				modifierSet.addIntModifier(Modifiers.FINAL);
-				markers.add(Markers.semantic(field.getPosition(), "field.trait.warning", field.getName()));
-			}
-		}
-
-		for (int i = 0, count = classBody.methodCount(); i < count; i++)
-		{
-			final IMethod method = classBody.getMethod(i);
-			if (!method.hasModifier(Modifiers.PUBLIC))
-			{
-				method.getModifiers().addIntModifier(Modifiers.PUBLIC);
-			}
-		}
-	}
-	
-	@Override
-	public void write(ClassWriter writer) throws BytecodeException
-	{
 	}
 }
