b094d0439d20336ccf2ac6a1d748327fb5429ebe
Clashsoft <clashsoft@hotmail.com>
2016-09-19 22:05:28 +0200

    Improve Operator Parsing
    comp: Improved Operator Parsing to treat more expressions as
    juxtaposition.
    comp: Improved Syntax errors for Colon and Assignment Operators.

--- src/parsing/dyvil/tools/parsing/position/ICodePosition.java
+++ src/parsing/dyvil/tools/parsing/position/ICodePosition.java
@@ -4,6 +4,24 @@ public interface ICodePosition
 {
 	ICodePosition ORIGIN = new CodePosition(1, 0, 1);
 
+	static ICodePosition before(ICodePosition next)
+	{
+		final int startLine = next.startLine();
+		int startIndex = next.startIndex();
+		if (startIndex == 0)
+		{
+			startIndex++;
+		}
+		return new CodePosition(startLine, startLine, startIndex - 1, startIndex);
+	}
+
+	static ICodePosition after(ICodePosition prev)
+	{
+		final int endLine = prev.endLine();
+		final int endIndex = prev.endIndex();
+		return new CodePosition(endLine, endLine, endIndex, endIndex + 1);
+	}
+
 	static ICodePosition between(ICodePosition start, ICodePosition end)
 	{
 		int startIndex = start.endIndex();
@@ -28,12 +46,12 @@ public interface ICodePosition
 
 	ICodePosition to(ICodePosition end);
 
-	default boolean before(ICodePosition position)
+	default boolean isBefore(ICodePosition position)
 	{
 		return this.endIndex() < position.startIndex();
 	}
 
-	default boolean after(ICodePosition position)
+	default boolean isAfter(ICodePosition position)
 	{
 		return this.startIndex() > position.endIndex();
 	}
