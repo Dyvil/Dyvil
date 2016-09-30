897af4029f4047dd3bf04fa5c075a2872e3dc26c
Clashsoft <clashsoft@hotmail.com>
2016-09-30 22:37:11 +0200

    Update String Extensions
    lib: Updated the `StringExtensions` class.
    lib: Updated the `StringUtils` class.
    (test): Updated the `StringTests` class.

--- src/test/dyvil/tests/StringTests.java
+++ src/test/dyvil/tests/StringTests.java
@@ -15,18 +15,6 @@ public class StringTests
 			"This Has Weird Casing", "Anothertest" };
 	
 	@Test
-	public void testAcronym()
-	{
-		assertEquals("Acronym of 'Hello World'", "HW", StringUtils.toAcronym("Hello World"));
-		assertEquals("Acronym of 'Dyvil Library'", "DL", StringUtils.toAcronym("Dyvil Library"));
-		assertEquals("Acronym of 'String Test'", "ST", StringUtils.toAcronym("String Test"));
-		assertEquals("Acronym of 'A Very Special Acronym'", "AVSA", StringUtils.toAcronym("A Very Special Acronym"));
-		assertEquals("Acronym of 'Abstract Syntax Tree'", "AST", StringUtils.toAcronym("Abstract Syntax Tree"));
-		assertEquals("Acronym of 'Half-Life 3'", "HL3", StringUtils.toAcronym("Half-Life 3"));
-		assertEquals("Acronym of 'Some-Random-Test'", "SRT", StringUtils.toAcronym("Some-Random-Test"));
-	}
-	
-	@Test
 	public void testTitleCase() throws Exception
 	{
 		for (int i = 0; i < cases.length; i++)
