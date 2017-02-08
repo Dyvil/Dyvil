115b23f1e8562f9e45d41ee9d275e270440edc31
Clashsoft <clashsoft@hotmail.com>
2017-02-08 22:54:15 +0100

    Clean up Library classes
    comp: Cleaned up the `Library` classes.

--- src/compiler/dyvil/tools/compiler/library/JarLibrary.java
+++ src/compiler/dyvil/tools/compiler/library/JarLibrary.java
@@ -3,39 +3,34 @@ package dyvil.tools.compiler.library;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 
 public final class JarLibrary extends Library
 {
 	private FileSystem jarFileSystem;
-	
+
 	public JarLibrary(File file)
 	{
 		super(file);
 	}
-	
+
 	@Override
 	public void loadLibrary()
 	{
-		URI fileURI = this.file.toURI();
 		try
 		{
-			URI uri = new URI("jar:" + fileURI);
-			this.jarFileSystem = FileSystems.newFileSystem(uri, Collections.EMPTY_MAP);
+			this.jarFileSystem = FileSystems.newFileSystem(this.file.toPath(), null);
 		}
 		catch (Exception ex)
 		{
 			System.err.println("Failed to initialize JAR library " + this.file);
-			System.err.println("URI: " + fileURI);
 			ex.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void unloadLibrary()
 	{
@@ -47,27 +42,29 @@ public final class JarLibrary extends Library
 		{
 		}
 	}
-	
+
 	@Override
-	public boolean isSubPackage(String name)
+	public boolean isSubPackage(String internal)
 	{
-		return Files.exists(this.jarFileSystem.getPath(name), emptyLinkOptions);
+		return Files.exists(this.jarFileSystem.getPath(internal), emptyLinkOptions);
 	}
-	
+
 	@Override
 	public InputStream getInputStream(String fileName)
 	{
-		Path path = this.jarFileSystem.getPath(fileName);
-		if (Files.exists(path, emptyLinkOptions))
+		final Path path = this.jarFileSystem.getPath(fileName);
+		if (!Files.exists(path, emptyLinkOptions))
 		{
-			try
-			{
-				return Files.newInputStream(path, emptyLinkOptions);
-			}
-			catch (IOException ex)
-			{
-			}
+			return null;
 		}
-		return null;
+
+		try
+		{
+			return Files.newInputStream(path, emptyLinkOptions);
+		}
+		catch (IOException ignored)
+		{
+			return null;
+		}
 	}
 }
