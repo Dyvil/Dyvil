c66e9729cb98585a4aa308a7fbd182bea6da2e1a
Clashsoft <clashsoft@hotmail.com>
2017-04-05 22:33:46 +0200

    Update Union and Intersection Type Classes
    comp: Updated the `UnionType` and `IntersectionType` classes and added
    a common superclass.
    comp: Improved Static and Implicit Method resolution for Union Types.
    comp: Fixed undeterministic Union and Intersection Type Erasure.

---
+++ src/compiler/dyvil/tools/compiler/ast/type/compound/BinaryType.java
@@ -0,0 +1,246 @@
+package dyvil.tools.compiler.ast.type.compound;
+
+import dyvil.collection.Set;
+import dyvil.tools.asm.TypeAnnotatableVisitor;
+import dyvil.tools.asm.TypePath;
+import dyvil.tools.compiler.ast.annotation.AnnotationUtil;
+import dyvil.tools.compiler.ast.annotation.IAnnotation;
+import dyvil.tools.compiler.ast.classes.IClass;
+import dyvil.tools.compiler.ast.constructor.IConstructor;
+import dyvil.tools.compiler.ast.context.IContext;
+import dyvil.tools.compiler.ast.generic.ITypeContext;
+import dyvil.tools.compiler.ast.generic.ITypeParameter;
+import dyvil.tools.compiler.ast.header.IClassCompilableList;
+import dyvil.tools.compiler.ast.header.ICompilableList;
+import dyvil.tools.compiler.ast.method.IMethod;
+import dyvil.tools.compiler.ast.method.MatchList;
+import dyvil.tools.compiler.ast.parameter.ArgumentList;
+import dyvil.tools.compiler.ast.type.IType;
+import dyvil.tools.compiler.ast.type.builtin.Types;
+import dyvil.tools.compiler.ast.type.raw.IObjectType;
+import dyvil.tools.compiler.util.MemberSorter;
+import dyvil.tools.parsing.ast.IASTNode;
+import dyvil.tools.parsing.marker.MarkerList;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Arrays;
+
+public abstract class BinaryType implements IObjectType
+{
+	private static final IClass[] OBJECT_CLASS_ARRAY = { Types.OBJECT_CLASS };
+
+	protected IType left;
+	protected IType right;
+
+	// Metadata
+	protected IClass[] commonClasses;
+
+	protected abstract IType combine(IType left, IType right);
+
+	public IType getLeft()
+	{
+		return this.left;
+	}
+
+	public void setLeft(IType left)
+	{
+		this.left = left;
+	}
+
+	public IType getRight()
+	{
+		return this.right;
+	}
+
+	public void setRight(IType right)
+	{
+		this.right = right;
+	}
+
+	@Override
+	public IType asParameterType()
+	{
+		return this.combine(this.left.asParameterType(), this.right.asParameterType());
+	}
+
+	@Override
+	public boolean isGenericType()
+	{
+		return this.left.isGenericType() || this.right.isGenericType();
+	}
+
+	@Override
+	public boolean isResolved()
+	{
+		return true;
+	}
+
+	@Override
+	public IClass getTheClass()
+	{
+		if (this.commonClasses != null)
+		{
+			return this.commonClasses[0];
+		}
+
+		if (this.left.getTheClass() == Types.OBJECT_CLASS || this.right.getTheClass() == Types.OBJECT_CLASS)
+		{
+			this.commonClasses = OBJECT_CLASS_ARRAY;
+			return Types.OBJECT_CLASS;
+		}
+
+		final Set<IClass> commonClassSet = Types.commonClasses(this.left, this.right);
+		commonClassSet.remove(Types.OBJECT_CLASS);
+
+		if (commonClassSet.isEmpty())
+		{
+			this.commonClasses = OBJECT_CLASS_ARRAY;
+			return Types.OBJECT_CLASS;
+		}
+
+		this.commonClasses = new IClass[commonClassSet.size()];
+		commonClassSet.toArray(this.commonClasses);
+		Arrays.sort(this.commonClasses, MemberSorter.CLASS_COMPARATOR);
+		return this.commonClasses[0];
+	}
+
+	@Override
+	public boolean hasTypeVariables()
+	{
+		return this.left.hasTypeVariables() || this.right.hasTypeVariables();
+	}
+
+	@Override
+	public IType resolveType(ITypeParameter typeParameter)
+	{
+		final IType left = this.left.resolveType(typeParameter);
+		final IType right = this.right.resolveType(typeParameter);
+		if (left == null)
+		{
+			return right;
+		}
+		if (right == null)
+		{
+			return left;
+		}
+
+		return this.combine(left, right);
+	}
+
+	@Override
+	public IType getConcreteType(ITypeContext context)
+	{
+		final IType left = this.left.getConcreteType(context);
+		final IType right = this.right.getConcreteType(context);
+		if (left == this.left && right == this.right)
+		{
+			return this;
+		}
+
+		return this.combine(left, right);
+	}
+
+	@Override
+	public void inferTypes(IType concrete, ITypeContext typeContext)
+	{
+		this.left.inferTypes(concrete, typeContext);
+		this.right.inferTypes(concrete, typeContext);
+	}
+
+	@Override
+	public IAnnotation getAnnotation(IClass type)
+	{
+		final IAnnotation left = this.left.getAnnotation(type);
+		return left != null ? left : this.right.getAnnotation(type);
+	}
+
+	// Resolution
+
+	@Override
+	public void getConstructorMatches(MatchList<IConstructor> list, ArgumentList arguments)
+	{
+	}
+
+	@Override
+	public IMethod getFunctionalMethod()
+	{
+		return null;
+	}
+
+	// Phases
+
+	@Override
+	public void checkType(MarkerList markers, IContext context, int position)
+	{
+		this.left.checkType(markers, context, position);
+		this.right.checkType(markers, context, position);
+	}
+
+	@Override
+	public void check(MarkerList markers, IContext context)
+	{
+		this.left.check(markers, context);
+		this.right.check(markers, context);
+	}
+
+	@Override
+	public void foldConstants()
+	{
+		this.left.foldConstants();
+		this.right.foldConstants();
+	}
+
+	@Override
+	public void cleanup(ICompilableList compilableList, IClassCompilableList classCompilableList)
+	{
+		this.left.cleanup(compilableList, classCompilableList);
+		this.right.cleanup(compilableList, classCompilableList);
+	}
+
+	// Compilation
+
+	@Override
+	public String getInternalName()
+	{
+		return this.getTheClass().getInternalName();
+	}
+
+	@Override
+	public int getDescriptorKind()
+	{
+		return NAME_FULL;
+	}
+
+	@Override
+	public void addAnnotation(IAnnotation annotation, TypePath typePath, int step, int steps)
+	{
+	}
+
+	@Override
+	public void writeAnnotations(TypeAnnotatableVisitor visitor, int typeRef, String typePath)
+	{
+		AnnotationUtil.visitDyvilName(this, visitor, typeRef, typePath);
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException
+	{
+		IType.writeType(this.left, out);
+		IType.writeType(this.right, out);
+	}
+
+	@Override
+	public void read(DataInput in) throws IOException
+	{
+		this.left = IType.readType(in);
+		this.right = IType.readType(in);
+	}
+
+	@Override
+	public String toString()
+	{
+		return IASTNode.toString(this);
+	}
+}
--- src/compiler/dyvil/tools/compiler/ast/type/compound/IntersectionType.java
+++ src/compiler/dyvil/tools/compiler/ast/type/compound/IntersectionType.java
@@ -1,48 +1,23 @@
 package dyvil.tools.compiler.ast.type.compound;
 
-import dyvil.collection.Set;
 import dyvil.reflect.Opcodes;
 import dyvil.tools.asm.Type;
-import dyvil.tools.asm.TypeAnnotatableVisitor;
-import dyvil.tools.asm.TypePath;
-import dyvil.tools.compiler.ast.annotation.AnnotationUtil;
-import dyvil.tools.compiler.ast.annotation.IAnnotation;
-import dyvil.tools.compiler.ast.classes.IClass;
-import dyvil.tools.compiler.ast.constructor.IConstructor;
 import dyvil.tools.compiler.ast.context.IContext;
 import dyvil.tools.compiler.ast.expression.IValue;
 import dyvil.tools.compiler.ast.field.IDataMember;
-import dyvil.tools.compiler.ast.generic.ITypeContext;
-import dyvil.tools.compiler.ast.generic.ITypeParameter;
-import dyvil.tools.compiler.ast.header.IClassCompilableList;
-import dyvil.tools.compiler.ast.header.ICompilableList;
 import dyvil.tools.compiler.ast.method.IMethod;
 import dyvil.tools.compiler.ast.method.MatchList;
 import dyvil.tools.compiler.ast.parameter.ArgumentList;
 import dyvil.tools.compiler.ast.type.IType;
 import dyvil.tools.compiler.ast.type.builtin.Types;
-import dyvil.tools.compiler.ast.type.raw.IObjectType;
 import dyvil.tools.compiler.backend.MethodWriter;
 import dyvil.tools.compiler.backend.exception.BytecodeException;
 import dyvil.tools.compiler.transform.Names;
 import dyvil.tools.parsing.Name;
-import dyvil.tools.parsing.ast.IASTNode;
 import dyvil.tools.parsing.marker.MarkerList;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-
-public class IntersectionType implements IObjectType
+public class IntersectionType extends BinaryType
 {
-	private static final IClass[] OBJECT_CLASS_ARRAY = { Types.OBJECT_CLASS };
-
-	private IType left;
-	private IType right;
-
-	// Metadata
-	private IClass[] commonClasses;
-
 	public IntersectionType()
 	{
 	}
@@ -51,6 +26,12 @@ public class IntersectionType implements IObjectType
 	{
 		this.left = left;
 		this.right = right;
+	}
+
+	@Override
+	protected IType combine(IType left, IType right)
+	{
+		return combine(left, right, null);
 	}
 
 	public static IType combine(IType left, IType right, IntersectionType intersectionType)
@@ -66,7 +47,9 @@ public class IntersectionType implements IObjectType
 			return left;
 		}
 
-		return intersectionType != null ? intersectionType : new IntersectionType(left.getObjectType(), right.getObjectType());
+		return intersectionType != null ?
+			       intersectionType :
+			       new IntersectionType(left.getObjectType(), right.getObjectType());
 	}
 
 	@Override
@@ -75,72 +58,13 @@ public class IntersectionType implements IObjectType
 		return INTERSECTION;
 	}
 
-	public IType getLeft()
-	{
-		return this.left;
-	}
-
-	public void setLeft(IType left)
-	{
-		this.left = left;
-	}
-
-	public IType getRight()
-	{
-		return this.right;
-	}
-
-	public void setRight(IType right)
-	{
-		this.right = right;
-	}
-
-	@Override
-	public boolean isGenericType()
-	{
-		return this.left.isGenericType() || this.right.isGenericType();
-	}
-
 	@Override
 	public Name getName()
 	{
 		return Names.Intersection;
 	}
 
-	@Override
-	public IClass getTheClass()
-	{
-		if (this.commonClasses != null)
-		{
-			return this.commonClasses[0];
-		}
-
-		if (this.left.getTheClass() == Types.OBJECT_CLASS || this.right.getTheClass() == Types.OBJECT_CLASS)
-		{
-			this.commonClasses = OBJECT_CLASS_ARRAY;
-			return Types.OBJECT_CLASS;
-		}
-
-		final Set<IClass> commonClassSet = Types.commonClasses(this.left, this.right);
-		commonClassSet.remove(Types.OBJECT_CLASS);
-
-		if (commonClassSet.isEmpty())
-		{
-			this.commonClasses = OBJECT_CLASS_ARRAY;
-			return Types.OBJECT_CLASS;
-		}
-
-		this.commonClasses = new IClass[commonClassSet.size()];
-		commonClassSet.toArray(this.commonClasses);
-
-		return this.commonClasses[0];
-	}
-
-	@Override
-	public IType asParameterType()
-	{
-		return combine(this.left.asParameterType(), this.right.asParameterType(), null);
-	}
+	// Subtyping
 
 	@Override
 	public int subTypeCheckLevel()
@@ -172,54 +96,7 @@ public class IntersectionType implements IObjectType
 		return Types.isSuperClass(superType, this.left) || Types.isSuperClass(superType, this.right);
 	}
 
-	@Override
-	public IType resolveType(ITypeParameter typeParameter)
-	{
-		final IType left = this.left.resolveType(typeParameter);
-		final IType right = this.right.resolveType(typeParameter);
-		if (left == null)
-		{
-			return right;
-		}
-		if (right == null)
-		{
-			return left;
-		}
-
-		return combine(left, right, null);
-	}
-
-	@Override
-	public boolean hasTypeVariables()
-	{
-		return this.left.hasTypeVariables() || this.right.hasTypeVariables();
-	}
-
-	@Override
-	public IType getConcreteType(ITypeContext context)
-	{
-		final IType left = this.left.getConcreteType(context);
-		final IType right = this.right.getConcreteType(context);
-		if (left == this.left && right == this.right)
-		{
-			return this;
-		}
-
-		return combine(left, right, null);
-	}
-
-	@Override
-	public void inferTypes(IType concrete, ITypeContext typeContext)
-	{
-		this.left.inferTypes(concrete, typeContext);
-		this.right.inferTypes(concrete, typeContext);
-	}
-
-	@Override
-	public boolean isResolved()
-	{
-		return true;
-	}
+	// Phases
 
 	@Override
 	public IType resolveType(MarkerList markers, IContext context)
@@ -229,33 +106,7 @@ public class IntersectionType implements IObjectType
 		return combine(this.left, this.right, this);
 	}
 
-	@Override
-	public void checkType(MarkerList markers, IContext context, int position)
-	{
-		this.left.checkType(markers, context, position);
-		this.right.checkType(markers, context, position);
-	}
-
-	@Override
-	public void check(MarkerList markers, IContext context)
-	{
-		this.left.check(markers, context);
-		this.right.check(markers, context);
-	}
-
-	@Override
-	public void foldConstants()
-	{
-		this.left.foldConstants();
-		this.right.foldConstants();
-	}
-
-	@Override
-	public void cleanup(ICompilableList compilableList, IClassCompilableList classCompilableList)
-	{
-		this.left.cleanup(compilableList, classCompilableList);
-		this.right.cleanup(compilableList, classCompilableList);
-	}
+	// Resolution
 
 	@Override
 	public IDataMember resolveField(Name name)
@@ -282,29 +133,14 @@ public class IntersectionType implements IObjectType
 		this.right.getImplicitMatches(list, value, targetType);
 	}
 
-	@Override
-	public void getConstructorMatches(MatchList<IConstructor> list, ArgumentList arguments)
-	{
-	}
-
-	@Override
-	public IMethod getFunctionalMethod()
-	{
-		return null;
-	}
-
-	@Override
-	public String getInternalName()
-	{
-		return this.getTheClass().getInternalName();
-	}
+	// Compilation
 
 	@Override
 	public void appendDescriptor(StringBuilder buffer, int type)
 	{
 		if (type == NAME_FULL)
 		{
-			buffer.append('|');
+			buffer.append('&');
 			this.left.appendDescriptor(buffer, NAME_FULL);
 			this.right.appendDescriptor(buffer, NAME_FULL);
 			return;
@@ -324,36 +160,7 @@ public class IntersectionType implements IObjectType
 		                       false);
 	}
 
-	@Override
-	public void addAnnotation(IAnnotation annotation, TypePath typePath, int step, int steps)
-	{
-	}
-
-	@Override
-	public void writeAnnotations(TypeAnnotatableVisitor visitor, int typeRef, String typePath)
-	{
-		AnnotationUtil.visitDyvilName(this, visitor, typeRef, typePath);
-	}
-
-	@Override
-	public void write(DataOutput out) throws IOException
-	{
-		IType.writeType(this.left, out);
-		IType.writeType(this.right, out);
-	}
-
-	@Override
-	public void read(DataInput in) throws IOException
-	{
-		this.left = IType.readType(in);
-		this.right = IType.readType(in);
-	}
-
-	@Override
-	public String toString()
-	{
-		return IASTNode.toString(this);
-	}
+	// String Conversion
 
 	@Override
 	public void toString(String prefix, StringBuilder buffer)
