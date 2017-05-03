0d5c60ea4301db4d5c4888e66de63a2bdee6a70d
Clashsoft <clashsoft@hotmail.com>
2017-05-03 13:57:24 +0200

    New GenSrc Lexer
    gensrc: Added the new `GenSrcLexer` class.

---
+++ src/gensrc/dyvil/tools/gensrc/lexer/GenSrcLexer.java
@@ -0,0 +1,245 @@
+package dyvil.tools.gensrc.lexer;
+
+import dyvil.tools.gensrc.ast.Util;
+import dyvil.tools.parsing.Name;
+import dyvil.tools.parsing.TokenList;
+import dyvil.tools.parsing.lexer.*;
+import dyvil.tools.parsing.marker.MarkerList;
+import dyvil.tools.parsing.token.IdentifierToken;
+import dyvil.tools.parsing.token.StringToken;
+import dyvil.tools.parsing.token.SymbolToken;
+
+import static dyvil.tools.parsing.lexer.BaseSymbols.*;
+
+public class GenSrcLexer extends dyvil.tools.parsing.lexer.Lexer
+{
+	protected int braceLevel;
+	protected final int blockLevel;
+
+	public GenSrcLexer(MarkerList markers)
+	{
+		this(markers, 0);
+	}
+
+	public GenSrcLexer(MarkerList markers, int blockLevel)
+	{
+		super(markers, BaseSymbols.INSTANCE);
+		this.blockLevel = blockLevel;
+	}
+
+	@Override
+	public TokenList tokenize(String code, int cursor, int line, int column)
+	{
+		this.init(code, cursor, line, column);
+
+		loop:
+		while (true)
+		{
+			final int currentChar = this.codePoint();
+			switch (currentChar)
+			{
+			case 0:
+				break loop;
+			case '}':
+				if (this.braceLevel <= 0)
+				{
+					break loop;
+				}
+			}
+
+			this.parseCharacter(currentChar);
+		}
+
+		this.finish();
+		return this.tokens;
+	}
+
+	@Override
+	protected void parseCharacter(int i)
+	{
+		switch (i)
+		{
+		case '#':
+			this.parseDirective();
+			break;
+		default:
+			this.parsePlain();
+			break;
+		}
+	}
+
+	private void parsePlain()
+	{
+		// skip leading tabs
+		for (int i = 0; i < this.blockLevel; i++)
+		{
+			if (this.codePoint() != '\t')
+			{
+				break;
+			}
+
+			this.advance();
+		}
+
+		final int startIndex = this.cursor;
+		final int startColumn = this.column;
+		final int startLine = this.line;
+
+		loop:
+		while (true)
+		{
+			int current = this.codePoint();
+
+			switch (current)
+			{
+			case 0:
+				break loop;
+			case '#':
+				if (this.nextCodePoint() != '#')
+				{
+					break loop;
+				}
+				this.advance();
+				break;
+			case '{':
+				this.braceLevel++;
+				break;
+			case '}':
+				if (this.braceLevel >= 0 && --this.braceLevel <= 0)
+				{
+					break loop;
+				}
+				break;
+			case '\n':
+				this.advance(); // end at newline, but include it
+				break loop;
+			}
+
+			this.advance(current);
+		}
+
+		if (startIndex == this.cursor)
+		{
+			return;
+		}
+
+		this.tokens.append(
+			new StringToken(this.code.substring(startIndex, this.cursor), Tokens.STRING, startLine, this.line,
+			                startColumn, this.column));
+	}
+
+	private void parseDirective()
+	{
+		this.tokens.append(new SymbolToken(BaseSymbols.INSTANCE, BaseSymbols.HASH, this.line, this.column));
+
+		this.advance();
+
+		this.parseIdentifier();
+
+		//noinspection StatementWithEmptyBody
+		while (this.parseArguments())
+			;
+	}
+
+	private boolean parseArguments()
+	{
+		int current = this.codePoint();
+
+		// if there exists a ( or { after optional whitespace ...
+		final int indexOfNonWhite = Util.skipWhitespace(this.code, this.cursor, this.length);
+		switch (this.code.codePointAt(indexOfNonWhite))
+		{
+		case '(':
+			// ... skip the whitespace
+			this.skipWhitespace(current);
+
+			this.tokens.append(new SymbolToken(BaseSymbols.INSTANCE, OPEN_PARENTHESIS, this.line, this.column));
+			this.advance();
+
+			this.parseDyvilArguments();
+
+			current = this.codePoint();
+			if (current == 0)
+			{
+				return false;
+			}
+
+			assert current == ')';
+			this.tokens.append(new SymbolToken(BaseSymbols.INSTANCE, CLOSE_PARENTHESIS, this.line, this.column));
+			this.advance();
+			return true;
+		case '{':
+			this.skipWhitespace(current);
+
+			this.tokens.append(new SymbolToken(BaseSymbols.INSTANCE, OPEN_CURLY_BRACKET, this.line, this.column));
+			this.advance();
+
+			this.parseNestedBlock();
+
+			current = this.codePoint();
+			if (current == 0)
+			{
+				return false;
+			}
+
+			assert current == '}';
+			this.tokens.append(new SymbolToken(BaseSymbols.INSTANCE, CLOSE_CURLY_BRACKET, this.line, this.column));
+			this.advance();
+			return true;
+		case 0:
+		default:
+			return false;
+		}
+	}
+
+	private void skipWhitespace(int current)
+	{
+		while (current != 0 && Character.isWhitespace(current))
+		{
+			this.advance(current);
+			current = this.codePoint();
+		}
+	}
+
+	private void parseIdentifier()
+	{
+		int startColumn = this.column;
+		int startIndex = this.cursor;
+
+		int current = this.codePoint();
+		while (current != 0 && LexerUtil.isIdentifierPart(current))
+		{
+			this.advance(current);
+			current = this.codePoint();
+		}
+
+		final String identifier = this.code.substring(startIndex, this.cursor);
+		this.tokens.append(
+			new IdentifierToken(Name.fromQualified(identifier), Tokens.LETTER_IDENTIFIER, this.line, startColumn,
+			                    this.column));
+	}
+
+	private void parseDyvilArguments()
+	{
+		final DyvilLexer sublexer = new DyvilLexer(this.markers, BaseSymbols.INSTANCE);
+		sublexer.setInterpolationEnd();
+
+		this.useSubLexer(sublexer);
+	}
+
+	private void parseNestedBlock()
+	{
+		final GenSrcLexer sublexer = new GenSrcLexer(this.markers, this.blockLevel + 1);
+		this.useSubLexer(sublexer);
+	}
+
+	private void useSubLexer(Lexer sublexer)
+	{
+		final TokenList tokens = sublexer.tokenize(this.code, this.cursor, this.line, this.column);
+		this.tokens.addAll(tokens);
+
+		this.cursor = sublexer.getCursor();
+		this.line = sublexer.getLine();
+		this.column = sublexer.getColumn();
+	}
+}
