842a9ae0f5e30ad2a427f5cedf63905137aaa98a
Clashsoft <clashsoft@hotmail.com>
2017-10-14 15:25:47 +0200

    Improve Semicolon Inference
    comp: Improved Semicolon Inference Rules.
    lang: Slightly altered Semicolon Inference behaviour.

--- src/compiler/dyvilx/tools/compiler/parser/SemicolonInference.java
+++ src/compiler/dyvilx/tools/compiler/parser/SemicolonInference.java
@@ -11,121 +11,107 @@ public final class SemicolonInference
 	{
 		// no instances
 	}
-	
+
 	public static void inferSemicolons(IToken first)
 	{
 		if (first == null)
 		{
 			return;
 		}
-		
+
 		IToken prev = first;
 		IToken next = first.next();
 		while (next.type() != Tokens.EOF)
 		{
 			next.setPrev(prev);
-			inferSemicolon(prev, next);
+			if (inferSemicolon(prev, next))
+			{
+				final IToken semicolon = new InferredSemicolon(prev.endLine(), prev.endColumn());
+				semicolon.setNext(next);
+				semicolon.setPrev(prev);
+				next.setPrev(semicolon);
+				prev.setNext(semicolon);
+			}
 			prev = next;
 			next = next.next();
 		}
 	}
-	
-	private static void inferSemicolon(IToken prev, IToken next)
+
+	private static boolean inferSemicolon(IToken prev, IToken next)
 	{
 		final int prevLine = prev.endLine();
 		final int nextLine = next.startLine();
 		if (nextLine == prevLine)
 		{
-			// Don't infer a semicolon
-			return;
+			// Obviously never infer a semicolon between tokens on the same line
+			return false;
 		}
-		
-		// Only check tokens if the two tokens are on adjacent lines. Always
-		// infer a semicolon if there are one or more blank lines in between
-		if (nextLine == prevLine + 1)
+
+		if (nextLine != prevLine + 1)
 		{
-			// Check last token on line in question
-			final int prevType = prev.type();
+			// Always infer a semicolon if there are one or more blank lines in between
+			return true;
+		}
 
-			// Check if the previous token is a symbol
-			if ((prevType & Tokens.SYMBOL) != 0)
-			{
-				switch (prevType)
-				{
-				case DyvilSymbols.HASH:
-				case DyvilSymbols.UNDERSCORE:
-				case DyvilSymbols.ELLIPSIS:
-					break; // continue inference checking
-				default:
-					return; // don't infer a semicolon
-				}
-			}
-			// Check if the previous token is a keyword, but not a value keyword
-			else if ((prevType & Tokens.KEYWORD) != 0)
-			{
-				switch (prevType)
-				{
-				case DyvilKeywords.TRUE:
-				case DyvilKeywords.FALSE:
-				case DyvilKeywords.BREAK:
-				case DyvilKeywords.CONTINUE:
-				case DyvilKeywords.RETURN:
-				case DyvilKeywords.THIS:
-				case DyvilKeywords.SUPER:
-				case DyvilKeywords.NIL:
-				case DyvilKeywords.NULL:
-					break; // continue inference checking
-				default:
-					return; // don't infer a semicolon
-				}
-			}
-			else
-			{
-				// Check for other token types
-				switch (prevType)
-				{
-				case Tokens.STRING_START:
-				case Tokens.STRING_PART:
-				case BaseSymbols.OPEN_PARENTHESIS:
-				case BaseSymbols.OPEN_SQUARE_BRACKET:
-				case BaseSymbols.OPEN_CURLY_BRACKET:
-					return;
-				}
-			}
+		// Check last token on line in question
+		final int prevType = prev.type();
 
-			// Check first token on the next line
-
-			final int nextType = next.type();
-			// Check if the first token on the next line is a symbol
-			if ((nextType & Tokens.SYMBOL) != 0)
+		if ((prevType & Tokens.KEYWORD) == 0)
+		{
+			// Don't infer a semicolon for any of the following token types:
+			switch (prevType)
 			{
-				switch (nextType)
-				{
-				case DyvilSymbols.AT:
-				case DyvilSymbols.HASH:
-					break; // continue inference checking
-				default:
-					return; // don't infer a semicolon
-				}
-			}
-
-			// Check for other token types
-			switch (nextType)
-			{
+			case BaseSymbols.DOT:
+			case BaseSymbols.COMMA:
+			case BaseSymbols.SEMICOLON:
+			case Tokens.STRING_START:
 			case Tokens.STRING_PART:
-			case Tokens.STRING_END:
+			case BaseSymbols.OPEN_PARENTHESIS:
+			case BaseSymbols.OPEN_SQUARE_BRACKET:
 			case BaseSymbols.OPEN_CURLY_BRACKET:
-			case BaseSymbols.CLOSE_PARENTHESIS:
-			case BaseSymbols.CLOSE_SQUARE_BRACKET:
-			case BaseSymbols.CLOSE_CURLY_BRACKET:
-				return;
+				return false;
 			}
 		}
-		
-		final IToken semicolon = new InferredSemicolon(prevLine, prev.endColumn());
-		semicolon.setNext(next);
-		semicolon.setPrev(prev);
-		next.setPrev(semicolon);
-		prev.setNext(semicolon);
+		else
+		{
+			// Infer a semicolon if the previous token is a keyword, but not a value keyword
+			switch (prevType)
+			{
+			case DyvilKeywords.TRUE:
+			case DyvilKeywords.FALSE:
+			case DyvilKeywords.BREAK:
+			case DyvilKeywords.CONTINUE:
+			case DyvilKeywords.RETURN:
+			case DyvilKeywords.THIS:
+			case DyvilKeywords.SUPER:
+			case DyvilKeywords.NIL:
+			case DyvilKeywords.NULL:
+				break; // continue inference checking
+			default:
+				return false;
+			}
+		}
+
+		// Check first token on the next line
+
+		final int nextType = next.type();
+
+		// Check for other token types
+		switch (nextType)
+		{
+		case BaseSymbols.DOT:
+		case BaseSymbols.COMMA:
+		case BaseSymbols.SEMICOLON:
+		case BaseSymbols.EQUALS:
+		case BaseSymbols.COLON:
+		case BaseSymbols.OPEN_CURLY_BRACKET:
+		case BaseSymbols.CLOSE_PARENTHESIS:
+		case BaseSymbols.CLOSE_SQUARE_BRACKET:
+		case BaseSymbols.CLOSE_CURLY_BRACKET:
+		case Tokens.STRING_PART:
+		case Tokens.STRING_END:
+			return false;
+		}
+		return true;
 	}
 }
