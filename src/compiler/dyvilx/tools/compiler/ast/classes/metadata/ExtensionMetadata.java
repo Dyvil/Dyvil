031a002b0d7ee46fd897c9dd8803bf48622ba8a3
Adrian Kunz <clashsoft@hotmail.com>
2018-07-25 20:50:36 +0200

    Transform and Check Extension Classes
    comp: Extension Classes now automatically add the `extension` flag to all methods. #304
    comp: Extension Classes now disallow fields and properties. #304

--- src/compiler/dyvilx/tools/compiler/ast/classes/metadata/ExtensionMetadata.java
+++ src/compiler/dyvilx/tools/compiler/ast/classes/metadata/ExtensionMetadata.java
@@ -1,14 +1,21 @@
 package dyvilx.tools.compiler.ast.classes.metadata;
 
 import dyvil.lang.Name;
+import dyvil.reflect.Modifiers;
+import dyvilx.tools.compiler.ast.classes.ClassBody;
 import dyvilx.tools.compiler.ast.classes.IClass;
 import dyvilx.tools.compiler.ast.context.IContext;
+import dyvilx.tools.compiler.ast.field.IField;
+import dyvilx.tools.compiler.ast.field.IProperty;
 import dyvilx.tools.compiler.ast.header.IClassCompilableList;
 import dyvilx.tools.compiler.ast.header.ICompilableList;
 import dyvilx.tools.compiler.ast.member.MemberKind;
+import dyvilx.tools.compiler.ast.method.IMethod;
+import dyvilx.tools.compiler.ast.type.IType;
 import dyvilx.tools.compiler.ast.type.builtin.Types;
 import dyvilx.tools.compiler.backend.classes.ClassWriter;
 import dyvilx.tools.compiler.backend.exception.BytecodeException;
+import dyvilx.tools.compiler.util.Markers;
 import dyvilx.tools.parsing.marker.MarkerList;
 
 public class ExtensionMetadata implements IClassMetadata
@@ -34,6 +41,29 @@ public class ExtensionMetadata implements IClassMetadata
 		this.theClass.setPosition(this.theClass.getSuperType().getPosition());
 		this.theClass.setName(
 			Name.fromQualified("extension_" + this.theClass.getSuperType().getInternalName().replace('/', '_')));
+	}
+
+	@Override
+	public void resolveTypesBody(MarkerList markers, IContext context)
+	{
+		final ClassBody body = this.theClass.getBody();
+
+		for (IField field : body.fields())
+		{
+			markers.add(Markers.semanticError(field.getPosition(), "extension.field.invalid"));
+		}
+
+		for (IProperty property : body.properties())
+		{
+			markers.add(Markers.semanticError(property.getPosition(), "extension.property.invalid"));
+		}
+
+		final IType superType = this.theClass.getSuperType();
+		for (IMethod method : body.methods())
+		{
+			method.setThisType(superType);
+			method.getAttributes().addFlag(Modifiers.EXTENSION);
+		}
 	}
 
 	@Override
