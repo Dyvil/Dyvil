99ce8d6356cde76d7ce942637e0dd3031f9c76a0
Adrian Kunz <clashsoft@hotmail.com>
2018-09-29 15:24:07 +0200

    Clean up SequentialCompilerPhase
    comp: Cleaned up the `SequentialCompilerPhase` class.
    comp: The `SequentialCompilerPhase` implementation now catches exceptions on a per-unit basis.

--- src/compiler/dyvilx/tools/compiler/phase/SequentialCompilerPhase.java
+++ src/compiler/dyvilx/tools/compiler/phase/SequentialCompilerPhase.java
@@ -3,46 +3,84 @@ package dyvilx.tools.compiler.phase;
 import dyvil.annotation.internal.NonNull;
 import dyvilx.tools.compiler.DyvilCompiler;
 import dyvilx.tools.compiler.ast.header.ICompilationUnit;
+import dyvilx.tools.compiler.lang.I18n;
 
 import java.util.function.Consumer;
 
 public class SequentialCompilerPhase implements ICompilerPhase
 {
-	private final int                        id;
-	private final String                     name;
+	// =============== Fields ===============
+
+	private final int    id;
+	private final String name;
+
 	private final Consumer<ICompilationUnit> apply;
-	
+
+	// =============== Constructors ===============
+
 	public SequentialCompilerPhase(int id, String name, Consumer<ICompilationUnit> apply)
 	{
 		this.id = id;
 		this.name = name;
 		this.apply = apply;
 	}
-	
+
+	// =============== Properties ===============
+
 	@Override
 	public String getName()
 	{
 		return this.name;
 	}
-	
+
 	@Override
 	public int getID()
 	{
 		return this.id;
 	}
-	
+
+	// =============== Methods ===============
+
+	// --------------- Phase Apply ---------------
+
 	@Override
 	public void apply(DyvilCompiler compiler)
 	{
-		compiler.fileFinder.units.forEach(this.apply);
+		for (ICompilationUnit unit : compiler.fileFinder.units)
+		{
+			try
+			{
+				this.apply.accept(unit);
+			}
+			catch (Exception e)
+			{
+				compiler.error(I18n.get("phase.failed.unit", this.getName(), unit.getFileSource().file()), e);
+			}
+		}
 	}
-	
+
+	// --------------- Comparison, Equals and Hash Code ---------------
+
 	@Override
 	public int compareTo(@NonNull ICompilerPhase o)
 	{
 		return Integer.compare(this.id, o.getID());
 	}
-	
+
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof ICompilerPhase && this.getID() == ((ICompilerPhase) o).getID();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return this.getID();
+	}
+
+	// --------------- Formatting ---------------
+
 	@Override
 	public String toString()
 	{
