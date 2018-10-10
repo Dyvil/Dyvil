9038586d10a27d631124214f38ee556d9f6eed8d
Adrian Kunz <clashsoft@hotmail.com>
2018-10-10 23:53:23 +0200

    Fix Type Parser
    comp: The Type Parser now properly reports when it couldn't parse a type. #373, #407, #417
    comp: Cleaned up the `ExpressionListParser`, `TypeListParser` and `TypeParser` classes.
    
    Closes #373, #407, #417

--- src/compiler/dyvilx/tools/compiler/parser/expression/ExpressionListParser.java
+++ src/compiler/dyvilx/tools/compiler/parser/expression/ExpressionListParser.java
@@ -1,26 +1,32 @@
 package dyvilx.tools.compiler.parser.expression;
 
 import dyvilx.tools.compiler.ast.consumer.IValueConsumer;
-import dyvilx.tools.compiler.ast.expression.IValue;
-import dyvilx.tools.compiler.ast.expression.IValueList;
 import dyvilx.tools.parsing.IParserManager;
 import dyvilx.tools.parsing.Parser;
 import dyvilx.tools.parsing.lexer.BaseSymbols;
 import dyvilx.tools.parsing.token.IToken;
 
-public final class ExpressionListParser extends Parser implements IValueConsumer
+public class ExpressionListParser extends Parser
 {
+	// =============== Constants ===============
+
 	private static final int EXPRESSION = 0;
 	private static final int COMMA      = 1;
 
-	protected IValueList valueList;
-	
-	public ExpressionListParser(IValueList valueList)
+	// =============== Fields ===============
+
+	protected IValueConsumer consumer;
+
+	// =============== Constructors ===============
+
+	public ExpressionListParser(IValueConsumer consumer)
 	{
-		this.valueList = valueList;
+		this.consumer = consumer;
 		this.mode = EXPRESSION;
 	}
-	
+
+	// =============== Methods ===============
+
 	@Override
 	public void parse(IParserManager pm, IToken token)
 	{
@@ -30,27 +36,29 @@ public final class ExpressionListParser extends Parser implements IValueConsumer
 			pm.popParser(true);
 			return;
 		}
-		
+
 		switch (this.mode)
 		{
 		case EXPRESSION:
 			this.mode = COMMA;
-			pm.pushParser(new ExpressionParser(this), true);
+			pm.pushParser(new ExpressionParser(this.consumer), true);
 			return;
 		case COMMA:
-			if (type == BaseSymbols.COMMA || type == BaseSymbols.SEMICOLON && token.isInferred())
+			switch (type)
 			{
+			case BaseSymbols.SEMICOLON:
+				if (!token.isInferred())
+				{
+					pm.report(token, "expression.list.comma");
+				}
+				// Fallthrough
+			case BaseSymbols.COMMA:
 				this.mode = EXPRESSION;
 				return;
+			default:
+				pm.report(token, "expression.list.comma");
+				return;
 			}
-			pm.report(token, "expression.list.comma");
-			return;
 		}
-	}
-	
-	@Override
-	public void setValue(IValue value)
-	{
-		this.valueList.add(value);
 	}
 }
