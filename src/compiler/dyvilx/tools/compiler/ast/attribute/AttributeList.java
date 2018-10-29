f09254440f19fbf6a1cd97fee552e9af160ed8a4
Adrian Kunz <clashsoft@hotmail.com>
2018-10-29 21:44:15 +0100

    Clean up AttributeList
    comp: Made the `AttributeList` class extend `ArrayList` and `ResolvableList` and removed the manual list implementation. #439
    comp: Cleaned up the `AttributeList` class.
    comp: The `AttributeList` class now distinguishes between attribute-derived and custom flags.

--- src/compiler/dyvilx/tools/compiler/ast/attribute/AttributeList.java
+++ src/compiler/dyvilx/tools/compiler/ast/attribute/AttributeList.java
@@ -1,8 +1,6 @@
 package dyvilx.tools.compiler.ast.attribute;
 
 import dyvil.annotation.internal.NonNull;
-import dyvil.annotation.internal.Nullable;
-import dyvil.collection.iterator.ArrayIterator;
 import dyvilx.tools.asm.AnnotatableVisitor;
 import dyvilx.tools.asm.TypeAnnotatableVisitor;
 import dyvilx.tools.asm.TypePath;
@@ -10,23 +8,26 @@ import dyvilx.tools.compiler.ast.attribute.annotation.Annotation;
 import dyvilx.tools.compiler.ast.attribute.annotation.ExternalAnnotation;
 import dyvilx.tools.compiler.ast.classes.IClass;
 import dyvilx.tools.compiler.ast.context.IContext;
-import dyvilx.tools.compiler.ast.header.IClassCompilableList;
-import dyvilx.tools.compiler.ast.header.ICompilableList;
 import dyvilx.tools.compiler.ast.type.IType;
+import dyvilx.tools.compiler.phase.ResolvableList;
 import dyvilx.tools.parsing.marker.MarkerList;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.lang.annotation.ElementType;
-import java.util.Iterator;
+import java.util.ArrayList;
 
-public class AttributeList implements Iterable<Attribute>
+public class AttributeList extends ArrayList<Attribute> implements ResolvableList<Attribute>
 {
-	protected Attribute @Nullable [] data;
+	// =============== Fields ===============
 
-	protected int  size;
-	protected long flags;
+	private long customFlags;
+
+	private int  cachedAttributeFlagsModCount;
+	private long cachedAttributeFlags;
+
+	// =============== Constructors ===============
 
 	public AttributeList()
 	{
@@ -34,77 +35,81 @@ public class AttributeList implements Iterable<Attribute>
 
 	public AttributeList(int capacity)
 	{
-		this.data = new Attribute[capacity];
+		super(capacity);
 	}
+
+	// =============== Static Methods ===============
 
 	public static AttributeList of(long flags)
 	{
 		AttributeList list = new AttributeList();
-		list.flags = flags;
+		list.customFlags = flags;
 		return list;
 	}
 
-	public int size()
-	{
-		return this.size;
-	}
-
-	public boolean isEmpty()
-	{
-		return this.size == 0;
-	}
-
-	@Override
-	public Iterator<Attribute> iterator()
-	{
-		return new ArrayIterator<>(this.data, 0, this.size);
-	}
-
-	// Flags
+	// =============== Properties ===============
 
 	public long flags()
 	{
-		return this.flags;
+		return this.customFlags | this.getAttributeFlags();
 	}
+
+	public long getAttributeFlags()
+	{
+		if (this.modCount == this.cachedAttributeFlagsModCount)
+		{
+			return this.cachedAttributeFlags;
+		}
+
+		long attributeFlags = 0;
+		for (Attribute attribute : this)
+		{
+			attributeFlags |= attribute.flags();
+		}
+
+		this.cachedAttributeFlagsModCount = this.modCount;
+		this.cachedAttributeFlags = attributeFlags;
+		return attributeFlags;
+	}
+
+	public long getCustomFlags()
+	{
+		return this.customFlags;
+	}
+
+	// =============== Methods ===============
+
+	// --------------- Flag Queries ---------------
 
 	public boolean hasFlag(long flag)
 	{
-		return (this.flags & flag) == flag;
+		return (this.flags() & flag) == flag;
 	}
 
 	public boolean hasAnyFlag(long flag)
 	{
-		return (this.flags & flag) != 0;
+		return (this.flags() & flag) != 0;
 	}
+
+	// --------------- Custom Flags ---------------
 
 	public void addFlag(long flag)
 	{
-		this.flags |= flag;
+		this.customFlags |= flag;
 	}
 
 	public void removeFlag(long flag)
 	{
-		this.flags &= ~flag;
+		this.customFlags &= ~flag;
 	}
 
-	// Attributes and Annotations
-
-	public Attribute get(int index)
-	{
-		if (this.data == null || index >= this.size)
-		{
-			return null;
-		}
-		return this.data[index];
-	}
+	// --------------- Annotations ---------------
 
 	public Annotation getAnnotation(IClass type)
 	{
-		for (int i = 0; i < this.size; i++)
+		for (Attribute attribute : this)
 		{
-			final Attribute attribute = this.data[i];
 			final IType attributeType = attribute.getType();
-
 			if (attributeType != null && attributeType.getTheClass() == type)
 			{
 				return (Annotation) attribute;
@@ -113,78 +118,18 @@ public class AttributeList implements Iterable<Attribute>
 		return null;
 	}
 
-	public final void add(Attribute attribute)
-	{
-		this.ensureCapacity(this.size + 1);
-		this.data[this.size++] = attribute;
-		this.flags |= attribute.flags();
-	}
-
-	private void ensureCapacity(int capacity)
-	{
-		if (this.data == null)
-		{
-			this.data = new Attribute[capacity];
-			return;
-		}
-		if (capacity > this.data.length)
-		{
-			final Attribute[] temp = new Attribute[capacity];
-			System.arraycopy(this.data, 0, temp, 0, this.size);
-			this.data = temp;
-		}
-	}
-
-	public void addAll(AttributeList list)
-	{
-		this.ensureCapacity(this.size + list.size);
-		System.arraycopy(list.data, 0, this.data, this.size, list.size);
-		this.flags |= list.flags;
-	}
-
 	public void addAnnotations(AttributeList list)
 	{
-		this.ensureCapacity(this.size + list.size);
-		for (int i = 0; i < list.size; i++)
+		for (Attribute attr : list)
 		{
-			final Attribute attribute = list.data[i];
-			if (attribute.getType() != null)
+			if (attr.getType() != null)
 			{
-				this.add(attribute);
+				this.add(attr);
 			}
 		}
 	}
 
-	private void removeAt(int index)
-	{
-		if (this.data == null || index >= this.size)
-		{
-			return;
-		}
-
-		final int moved = this.size - index - 1;
-		if (moved > 0)
-		{
-			System.arraycopy(this.data, index + 1, this.data, index, moved);
-		}
-		this.data[--this.size] = null;
-	}
-
-	public AttributeList filtered(long mask)
-	{
-		AttributeList copy = new AttributeList(this.size);
-		for (int i = 0; i < this.size; i++)
-		{
-			final Attribute attribute = this.data[i];
-			final long flags = attribute.flags();
-			if (flags == 0 || (flags & mask) != 0)
-			{
-				copy.add(attribute);
-			}
-		}
-		copy.flags |= this.flags & mask;
-		return copy;
-	}
+	// --------------- Filtered Copies ---------------
 
 	public AttributeList annotations()
 	{
@@ -193,84 +138,79 @@ public class AttributeList implements Iterable<Attribute>
 		return copy;
 	}
 
-	// Phases
+	public AttributeList filtered(long mask)
+	{
+		AttributeList copy = new AttributeList(this.size());
+		for (Attribute attribute : this)
+		{
+			final long flags = attribute.flags();
+			if (flags == 0 || (flags & mask) != 0)
+			{
+				copy.add(attribute);
+			}
+		}
+		copy.customFlags |= this.customFlags & mask;
+		return copy;
+	}
+
+	// --------------- Resolution Phases ---------------
+
+	@Override
+	public void resolveTypes(MarkerList markers, IContext context)
+	{
+		throw new UnsupportedOperationException("use resolveTypes(MarkerList, IContext, Attributable) instead");
+	}
 
 	public void resolveTypes(MarkerList markers, IContext context, Attributable annotated)
 	{
-		for (int i = 0; i < this.size; i++)
-		{
-			final Attribute attribute = this.data[i];
-			attribute.resolveTypes(markers, context);
+		ResolvableList.super.resolveTypes(markers, context);
 
+		this.removeIf(attribute -> {
 			final IType type = attribute.getType();
 			if (type != null)
 			{
 				final String internalName = type.getInternalName();
-				if (internalName != null && annotated.skipAnnotation(internalName, (Annotation) attribute))
-				{
-					this.removeAt(i--);
-				}
+				return internalName != null && annotated.skipAnnotation(internalName, (Annotation) attribute);
 			}
-		}
+			return false;
+		});
 	}
 
-	public void resolve(MarkerList markers, IContext context)
-	{
-		for (int i = 0; i < this.size; i++)
-		{
-			this.data[i].resolve(markers, context);
-		}
-	}
+	// --------------- Diagnostic Phases ---------------
 
-	public void checkTypes(MarkerList markers, IContext context)
+	@Override
+	public void check(MarkerList markers, IContext context)
 	{
-		for (int i = 0; i < this.size; i++)
-		{
-			this.data[i].checkTypes(markers, context);
-		}
+		throw new UnsupportedOperationException("use check(MarkerList, IContext, ElementType) instead");
 	}
 
 	public void check(MarkerList markers, IContext context, ElementType target)
 	{
-		for (int i = 0; i < this.size; i++)
+		for (Attribute attribute : this)
 		{
-			this.data[i].check(markers, context, target);
+			attribute.check(markers, context, target);
 		}
 	}
 
-	public void foldConstants()
-	{
-		for (int i = 0; i < this.size; i++)
-		{
-			this.data[i].foldConstants();
-		}
-	}
-
-	public void cleanup(ICompilableList compilableList, IClassCompilableList classCompilableList)
-	{
-		for (int i = 0; i < this.size; i++)
-		{
-			this.data[i].cleanup(compilableList, classCompilableList);
-		}
-	}
-
-	// Compilation
+	// --------------- Compilation ---------------
 
 	public void write(AnnotatableVisitor writer)
 	{
-		for (int i = 0; i < this.size; i++)
+		for (Attribute attribute : this)
 		{
-			this.data[i].write(writer);
+			attribute.write(writer);
 		}
 	}
 
 	public void write(TypeAnnotatableVisitor visitor, int typeRef, TypePath typePath)
 	{
-		for (int i = 0; i < this.size; i++)
+		for (Attribute attribute : this)
 		{
-			this.data[i].write(visitor, typeRef, typePath);
+			attribute.write(visitor, typeRef, typePath);
 		}
 	}
+
+	// --------------- Serialization ---------------
 
 	public static void write(AttributeList list, DataOutput out) throws IOException
 	{
@@ -281,21 +221,16 @@ public class AttributeList implements Iterable<Attribute>
 			return;
 		}
 
-		int annotations = 0;
-		// count the number of annotations
-		for (int i = 0; i < list.size; i++)
-		{
-			if (list.data[i].getType() != null)
-			{
-				annotations++;
-			}
-		}
+		final AttributeList annotations = list.annotations();
+		final int numAnnotations = annotations.size();
 
-		out.writeLong(list.flags);
-		out.writeShort(annotations);
-		for (int i = 0; i < annotations; i++)
+		out.writeLong(list.customFlags);
+		out.writeShort(numAnnotations);
+
+		//noinspection ForLoopReplaceableByForEach to avoid concurrency problems
+		for (int i = 0; i < numAnnotations; i++)
 		{
-			list.data[i].write(out);
+			annotations.get(i).write(out);
 		}
 	}
 
@@ -305,24 +240,26 @@ public class AttributeList implements Iterable<Attribute>
 		final int annotations = in.readShort();
 		final AttributeList list = new AttributeList(annotations);
 
-		list.flags = flags;
-		list.size = annotations;
+		list.customFlags = flags;
+		list.ensureCapacity(annotations);
 
 		for (int i = 0; i < annotations; i++)
 		{
 			final Annotation annotation = new ExternalAnnotation();
-			list.data[i] = annotation;
+			list.add(annotation);
 			annotation.read(in);
 		}
 
 		return list;
 	}
 
+	// --------------- Formatting ---------------
+
 	public void toInlineString(@NonNull String indent, @NonNull StringBuilder buffer)
 	{
-		for (int i = 0; i < this.size; i++)
+		for (Attribute attribute : this)
 		{
-			this.data[i].toString(indent, buffer);
+			attribute.toString(indent, buffer);
 			buffer.append(' ');
 		}
 	}
@@ -330,28 +267,28 @@ public class AttributeList implements Iterable<Attribute>
 	public void toString(@NonNull String indent, @NonNull StringBuilder buffer)
 	{
 		int i = 0;
+		final int size = this.size();
 
-		// add a newline after all leading attributes without an int flag
+		// add a newline after all leading attributes without flags
 		// as soon as we encounter one, we use spaces.
 
-		for (; i < this.size; i++)
+		while (i < size)
 		{
-			final Attribute attribute = this.data[i];
+			final Attribute attribute = this.get(i++);
 			attribute.toString(indent, buffer);
 
 			if (attribute.flags() != 0)
 			{
 				buffer.append(' ');
-				i++;
 				break; // continue in the other loop, that does not use newlines
 			}
 
 			buffer.append('\n').append(indent);
 		}
 
-		for (; i < this.size; i++)
+		while (i < size)
 		{
-			this.data[i].toString(indent, buffer);
+			this.get(i++).toString(indent, buffer);
 			buffer.append(' ');
 		}
 	}
