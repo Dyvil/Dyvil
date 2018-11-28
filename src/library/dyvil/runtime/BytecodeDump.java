d3d5393177b4489118dfa632226249326571fde9
Adrian Kunz <clashsoft@hotmail.com>
2018-11-28 22:32:45 +0100

    Fix infinite invokedynamic recursion in BytecodeDump
    lib: Fixed an infinite recursion issue caused by the `BytecodeDump` implementation.

--- src/library/dyvil/runtime/BytecodeDump.java
+++ src/library/dyvil/runtime/BytecodeDump.java
@@ -1,22 +1,37 @@
 package dyvil.runtime;
 
 import dyvil.annotation.internal.NonNull;
-import dyvil.io.Files;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class BytecodeDump
 {
-	private static final String dumpProperty = System.getProperty("dyvil.bytecode.dump");
-	private static final File dumpDirectory = dumpProperty == null ? null : new File(dumpProperty);
+	private static final String dumpProperty  = System.getProperty("dyvil.bytecode.dump");
+	private static final File   dumpDirectory = dumpProperty == null ? null : new File(dumpProperty);
 
-	@SuppressWarnings("ConstantConditions")
+	static
+	{
+		if (dumpDirectory != null)
+		{
+			//noinspection ResultOfMethodCallIgnored
+			dumpDirectory.mkdirs();
+		}
+	}
+
 	public static void dump(byte @NonNull [] bytes, @NonNull String className)
 	{
 		if (dumpDirectory != null)
 		{
 			File dumpFile = new File(dumpDirectory, className.replace('/', File.separatorChar).concat(".class"));
-			Files.tryWriteData(dumpFile, bytes);
+			try (FileOutputStream os = new FileOutputStream(dumpFile))
+			{
+				os.write(bytes);
+			}
+			catch (IOException ignored)
+			{
+			}
 		}
 	}
 }
