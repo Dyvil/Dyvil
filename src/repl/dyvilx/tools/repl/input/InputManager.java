11a23f3e423f788d75cb4cb8f3129f5f10964dc5
Adrian Kunz <clashsoft@hotmail.com>
2018-11-29 14:10:31 +0100

    Improve multiline input
    repl: Improved multiline input using a custom JLine `Parser`.
    repl: Pressing Tab at the start of a line now inserts a tab character.

--- src/repl/dyvilx/tools/repl/input/InputManager.java
+++ src/repl/dyvilx/tools/repl/input/InputManager.java
@@ -1,10 +1,10 @@
 package dyvilx.tools.repl.input;
 
-import dyvil.lang.Strings;
 import dyvilx.tools.repl.DyvilREPL;
 import dyvilx.tools.repl.context.Colorizer;
 import org.jline.builtins.Completers;
 import org.jline.reader.*;
+import org.jline.reader.impl.DefaultParser;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 import org.jline.utils.AttributedString;
@@ -30,8 +30,26 @@ public class InputManager
 	private static LineReader createLineReader() throws IOException
 	{
 		return LineReaderBuilder.builder().appName("Dyvil REPL").terminal(createTerminal())
-		                        .highlighter(createHighlighter()).completer(createCompleter())
-		                        .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true).build();
+		                        .highlighter(createHighlighter()).completer(createCompleter()).parser(createParser())
+		                        .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
+		                        .option(LineReader.Option.INSERT_TAB, true).build();
+	}
+
+	private static Parser createParser()
+	{
+		return new DefaultParser()
+		{
+			@Override
+			public ParsedLine parse(String line, int cursor, ParseContext context)
+			{
+				if (context != ParseContext.COMPLETE && !isValid(line, cursor))
+				{
+					throw new EOFError(-1, -1, "EOF");
+				}
+
+				return super.parse(line, cursor, context);
+			}
+		};
 	}
 
 	private static Highlighter createHighlighter()
@@ -55,115 +73,99 @@ public class InputManager
 
 	public String readInput()
 	{
-		final StringBuilder buffer = new StringBuilder();
+		try
+		{
+			return this.reader.readLine("> ");
+		}
+		catch (EndOfFileException | UserInterruptException ex)
+		{
+			return null;
+		}
+	}
 
+	private static boolean isValid(String line, int cursor)
+	{
 		int braceDepth = 0;
 		int parenDepth = 0;
 		int bracketDepth = 0;
 		byte mode = 0;
-		String prompt = "> ";
-		String input;
 
-		while (true)
+		outer:
+		for (int i = 0; i < cursor; i++)
 		{
-			try
+			final char c = line.charAt(i);
+
+			switch (c)
 			{
-				input = this.reader.readLine(prompt);
-			}
-			catch (EndOfFileException | UserInterruptException ex)
-			{
-				return null;
-			}
-
-			final int len = input.length();
-
-			outer:
-			for (int i = 0; i < len; i++)
-			{
-				final char c = input.charAt(i);
-
-				buffer.append(c);
-
-				switch (c)
+			case '`':
+				if (mode == 0 || mode == IDENTIFIER)
 				{
-				case '`':
-					if (mode == 0 || mode == IDENTIFIER)
-					{
-						mode ^= IDENTIFIER;
-					}
-					break;
-				case '"':
-					if (mode == 0 || mode == DOUBLE_STRING)
-					{
-						mode ^= DOUBLE_STRING;
-					}
-					break;
-				case '\'':
-					if (mode == 0 || mode == SINGLE_STRING)
-					{
-						mode ^= SINGLE_STRING;
-					}
-					break;
-				case '\\':
-					if ((mode & ESCAPE) != 0)
-					{
-						break;
-					}
-					if (mode == DOUBLE_STRING || mode == SINGLE_STRING)
-					{
-						mode |= ESCAPE;
-					}
-					continue outer;
-				case '{':
-					if (mode == 0)
-					{
-						braceDepth++;
-					}
-					break;
-				case '}':
-					if (mode == 0)
-					{
-						braceDepth--;
-					}
-					break;
-				case '(':
-					if (mode == 0)
-					{
-						parenDepth++;
-					}
-					break;
-				case ')':
-					if (mode == 0)
-					{
-						parenDepth--;
-					}
-					break;
-				case '[':
-					if (mode == 0)
-					{
-						bracketDepth++;
-					}
-					break;
-				case ']':
-					if (mode == 0)
-					{
-						bracketDepth--;
-					}
+					mode ^= IDENTIFIER;
+				}
+				break;
+			case '"':
+				if (mode == 0 || mode == DOUBLE_STRING)
+				{
+					mode ^= DOUBLE_STRING;
+				}
+				break;
+			case '\'':
+				if (mode == 0 || mode == SINGLE_STRING)
+				{
+					mode ^= SINGLE_STRING;
+				}
+				break;
+			case '\\':
+				if ((mode & ESCAPE) != 0)
+				{
 					break;
 				}
-
-				mode &= ~ESCAPE;
-			}
-
-			buffer.append('\n');
-			if (mode == 0 && braceDepth + parenDepth + bracketDepth <= 0)
-			{
+				if (mode == DOUBLE_STRING || mode == SINGLE_STRING)
+				{
+					mode |= ESCAPE;
+				}
+				continue outer;
+			case '{':
+				if (mode == 0)
+				{
+					braceDepth++;
+				}
+				break;
+			case '}':
+				if (mode == 0)
+				{
+					braceDepth--;
+				}
+				break;
+			case '(':
+				if (mode == 0)
+				{
+					parenDepth++;
+				}
+				break;
+			case ')':
+				if (mode == 0)
+				{
+					parenDepth--;
+				}
+				break;
+			case '[':
+				if (mode == 0)
+				{
+					bracketDepth++;
+				}
+				break;
+			case ']':
+				if (mode == 0)
+				{
+					bracketDepth--;
+				}
 				break;
 			}
 
-			prompt = "| " + Strings.repeat("    ", braceDepth); // 4 spaces
+			mode &= ~ESCAPE;
 		}
 
-		return buffer.toString();
+		return mode == 0 && braceDepth + parenDepth + bracketDepth <= 0;
 	}
 }
