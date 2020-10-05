ded063a7df10481b4a75ec80366c57d54f15552c
Adrian Kunz <clashsoft@hotmail.com>
2020-10-05 20:11:12 +0200

    remove(gensrc): Don't collect templates and specs in GenSrc

--- gensrc/src/main/java/dyvilx/tools/gensrc/GenSrc.java
+++ gensrc/src/main/java/dyvilx/tools/gensrc/GenSrc.java
@@ -2,18 +2,10 @@ package dyvilx.tools.gensrc;
 
 import dyvilx.tools.compiler.DyvilCompiler;
 import dyvilx.tools.compiler.sources.DyvilFileType;
-import dyvilx.tools.gensrc.ast.Template;
 import dyvilx.tools.gensrc.sources.GenSrcFileType;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GenSrc extends DyvilCompiler
 {
-	private List<File>     specs     = new ArrayList<>();
-	private List<Template> templates = new ArrayList<>();
-
 	@Override
 	protected void setupFileFinder()
 	{
@@ -22,15 +14,5 @@ public class GenSrc extends DyvilCompiler
 
 		this.fileFinder.registerFileType(GenSrcFileType.CODE_EXTENSION, DyvilFileType.DYVIL_UNIT);
 		this.fileFinder.registerFileType(GenSrcFileType.HEADER_EXTENSION, DyvilFileType.DYVIL_HEADER);
-	}
-
-	public void addSpec(File file)
-	{
-		this.specs.add(file);
-	}
-
-	public void addTemplate(Template template)
-	{
-		this.templates.add(template);
 	}
 }
